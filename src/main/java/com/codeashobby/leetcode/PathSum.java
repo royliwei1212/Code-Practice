596a2b704e587acb14f4a699a8f6ec9e74843317
Hao Zhou <codeashobby@gmail.com>
2015-04-22 22:23:05 -0400

    add path sum

---
+++ src/main/java/com/codeashobby/leetcode/PathSum.java
@@ -0,0 +1,41 @@
+package com.codeashobby.leetcode;
+
+/**
+ * Created by hzhou on 4/22/15. codeashobby@gmail.com
+ */
+public class PathSum {
+
+	public boolean hasPathSum(TreeNode root, int sum) {
+		if (root == null) {
+			return false;
+		}
+		return helper(root, sum);
+	}
+
+	private boolean helper(TreeNode root, int sum) {
+		if (root != null && root.left == null && root.right == null) {
+			return sum == root.val;
+		}
+		boolean left = false;
+		boolean right = false;
+		assert root != null;
+		if (root.left != null) {
+			left = helper(root.left, sum - root.val);
+		}
+		if (root.right != null) {
+			right = helper(root.right, sum - root.val);
+		}
+		return left || right;
+	}
+
+	public class TreeNode {
+
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}

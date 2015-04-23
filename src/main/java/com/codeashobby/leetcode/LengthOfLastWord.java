fbcb6be638a8aa0916a5b21a38ce72b1bf20cdb7
Hao Zhou <codeashobby@gmail.com>
2015-04-22 22:28:32 -0400

    add Length of Last Word

---
+++ src/main/java/com/codeashobby/leetcode/LengthOfLastWord.java
@@ -0,0 +1,22 @@
+package com.codeashobby.leetcode;
+
+/**
+ * Created by hzhou on 4/22/15. codeashobby@gmail.com
+ */
+public class LengthOfLastWord {
+
+	public int lengthOfLastWord(String s) {
+		if (s == null || s.trim().isEmpty()) {
+			return 0;
+		}
+		s = s.trim();
+		String[] splits = s.split(" ");
+		for (int i = splits.length - 1; i >= 0; i--) {
+			if (!splits[i].equals(" ")) {
+				return splits[i].length();
+			}
+		}
+
+		return 0;
+	}
+}

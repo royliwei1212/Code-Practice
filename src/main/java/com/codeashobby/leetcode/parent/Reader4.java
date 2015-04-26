02462dcd51357845797e6079bdaa62b99dd29fcf
Hao Zhou <royhzhou@gmail.com>
2015-04-26 15:18:06 -0400

    add ReadNCharactersGivenReadFour

---
+++ src/main/java/com/codeashobby/leetcode/parent/Reader4.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode.parent;
+
+/**
+ * Description
+ *
+ * @author hzhou
+ */
+public class Reader4 {
+
+	// this function is only used to remove the error from ReadNCharactersGivenReadFour
+	protected int read4(char[] buffer) {
+		return 1;
+	}
+}

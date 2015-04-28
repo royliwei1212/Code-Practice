3d5a02e46c448ee2b04222a2291813f46436d70f
Hao Zhou <codeashobby@gmail.com>
2015-04-27 20:40:43 -0400

    add MaximumDepthOfBinaryTree

---
+++ src/main/java/com/codeashobby/leetcode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+package com.codeashobby.leetcode;
+
+import com.codeashobby.leetcode.parent.TreeNode;
+
+/**
+ * Created by hzhou on 4/27/15. codeashobby@gmail.com
+ */
+public class MaximumDepthOfBinaryTree {
+
+	public int maxDepth(TreeNode root) {
+		return helper(root);
+	}
+
+	private int helper(TreeNode root) {
+		if (root == null) {
+			return 0;
+		} else {
+			return Math.max(helper(root.left), helper(root.right)) + 1;
+		}
+
+	}
+}

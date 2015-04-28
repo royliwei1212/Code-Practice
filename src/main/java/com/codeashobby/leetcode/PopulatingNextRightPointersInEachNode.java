8f6dbec753f86a1a60298e2c7d122f734f73a63b
Hao Zhou <codeashobby@gmail.com>
2015-04-27 23:29:02 -0400

    add PopulatingNextRightPointersInEachNode

---
+++ src/main/java/com/codeashobby/leetcode/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,35 @@
+package com.codeashobby.leetcode;
+
+import java.util.Stack;
+
+import com.codeashobby.leetcode.parent.TreeLinkNode;
+
+/**
+ * Created by hzhou on 4/27/15. codeashobby@gmail.com
+ */
+public class PopulatingNextRightPointersInEachNode {
+
+	public void connect(TreeLinkNode root) {
+		if (root == null) {
+			return;
+		}
+
+		Stack<TreeLinkNode> stack = new Stack<TreeLinkNode>();
+		stack.push(root);
+		while (!stack.isEmpty()) {
+			TreeLinkNode node = stack.pop();
+			if (node.left != null) {
+				stack.push(node.left);
+				TreeLinkNode cursor = null;
+				while (node != null) {
+					if (cursor != null) {
+						cursor.next = node.left;
+					}
+					node.left.next = node.right;
+					cursor = node.right;
+					node = node.next;
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/com/codeashobby/leetcode/parent/TreeLinkNode.java
@@ -0,0 +1,14 @@
+package com.codeashobby.leetcode.parent;
+
+/**
+ * Created by hzhou on 4/27/15. codeashobby@gmail.com
+ */
+public class TreeLinkNode {
+
+	public int val;
+	public TreeLinkNode left, right, next;
+
+	public TreeLinkNode(int x) {
+		val = x;
+	}
+}

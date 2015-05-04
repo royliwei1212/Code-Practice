ea886e55e22d09c4735c21da3b4fd6da683a5b1f
Hao Zhou <royhzhou@gmail.com>
2015-05-03 21:45:29 -0400

    add SpiralMatrixTwo

---
+++ src/main/java/com/codeashobby/leetcode/SpiralMatrixTwo.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Description
+ *
+ * @author hzhou
+ */
+public class SpiralMatrixTwo {
+
+	public int[][] generateMatrix(int n) {
+		if (n < 0) {
+			return null;
+		}
+		int[][] matrix = new int[n][n];
+		if (n == 0) {
+			return matrix;
+		}
+
+		int val = 1;
+
+		int x = 0;
+		int y = 0;
+		int w = n;
+		while (w > 0) {
+			// top
+			for (int i = 0; i < w - 1; i++) {
+				matrix[x][y++] = val++;
+			}
+
+			// right
+			for (int i = 0; i < w - 1; i++) {
+				matrix[x++][y] = val++;
+			}
+
+			// button
+			for (int i = 0; i < w - 1; i++) {
+				matrix[x][y--] = val++;
+			}
+
+			// right
+			for (int i = 0; i < w - 1; i++) {
+				matrix[x--][y] = val++;
+			}
+
+			w = w - 2;
+			x++;
+			y++;
+		}
+		if (n % 2 == 1) {
+			matrix[n / 2][n / 2] = val;
+		}
+		return matrix;
+	}
+
+	@Test
+	public void test() {
+		generateMatrix(1);
+	}
+}

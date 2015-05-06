2fd2f903f3f3500f0bc09478561b4fc23d48f759
Hao Zhou <codeashobby@gmail.com>
2015-05-05 21:10:42 -0400

    add powX

---
+++ src/main/java/com/codeashobby/leetcode/PowX.java
@@ -0,0 +1,22 @@
+package com.codeashobby.leetcode;
+
+/**
+ * Created by hzhou on 5/4/15. codeashobby@gmail.com
+ */
+public class PowX {
+
+	public double myPow(double x, int n) {
+		boolean isPositive = n > 0;
+		double result = 1.0;
+		n = n > 0 ? n : -n;
+		while (n > 0) {
+			if ((n & 1) == 1) {
+				result *= x;
+			}
+			x *= x;
+			n = n >> 1;
+		}
+
+		return isPositive ? result : (1.0 / result);
+	}
+}

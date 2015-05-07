b1e0dece189b293865c3294419f5d53b5ddff1f7
Hao Zhou <codeashobby@gmail.com>
2015-05-06 21:33:59 -0400

    add RemoveNthNodeFromEndOfList

---
+++ src/main/java/com/codeashobby/leetcode/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,40 @@
+package com.codeashobby.leetcode;
+
+import com.codeashobby.leetcode.parent.ListNode;
+
+/**
+ * Created by hzhou on 5/6/15. codeashobby@gmail.com
+ */
+public class RemoveNthNodeFromEndOfList {
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		if (head == null) {
+			return null;
+		}
+
+		if(n == 0) return head;
+
+		ListNode preHead = new ListNode(0);
+		preHead.next = head;
+		ListNode c1 = head;
+		ListNode c2 = head;
+		while (n > 0) {
+			c2 = c2.next;
+			n--;
+		}
+
+		if(c2 == null) {
+			return head.next;
+		}
+
+		while(c2.next != null) {
+			c1 = c1.next;
+			c2 = c2.next;
+		}
+
+		c1.next = c1.next.next;
+
+		return head;
+	}
+
+}

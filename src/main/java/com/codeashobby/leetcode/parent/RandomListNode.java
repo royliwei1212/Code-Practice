4f456ada6671324586fc9d3b393c4c713928b79c
Hao Zhou <royhzhou@gmail.com>
2015-05-10 15:46:33 -0400

    add CopyListWithRandomPointer

---
+++ src/main/java/com/codeashobby/leetcode/parent/RandomListNode.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode.parent;
+
+/**
+ * Description:
+ *
+ * @author hzhou
+ */
+public class RandomListNode {
+
+	public int label;
+	public RandomListNode next, random;
+
+	public RandomListNode(int x) {
+		this.label = x;
+	}
+}

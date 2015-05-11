87f9426a3e543193b48b44424e865bbb158502e3
Hao Zhou <royhzhou@gmail.com>
2015-05-10 21:14:34 -0400

    add RemoveDuplicatesFromSortedList

---
+++ src/main/java/com/codeashobby/leetcode/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode;
+
+import com.codeashobby.leetcode.parent.ListNode;
+
+/**
+ * Description:
+ *
+ * @author hzhou
+ */
+public class RemoveDuplicatesFromSortedList {
+
+	public ListNode deleteDuplicates(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		ListNode cursor, pre;
+		pre = head;
+		cursor = head.next;
+		while (cursor != null) {
+			while (cursor != null && cursor.val == pre.val) {
+				cursor = cursor.next;
+			}
+
+			if (cursor == null) {
+				pre.next = null;
+			} else {
+				pre.next = cursor;
+				pre = pre.next;
+				cursor = cursor.next;
+			}
+		}
+
+		return head;
+	}
+}

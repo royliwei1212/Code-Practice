096a81d20baedfd75a4f3e7df2e7ec6b02afb66f
Hao Zhou <codeashobby@gmail.com>
2015-05-12 22:31:44 -0400

    add MinimumSizeSubarraySum

---
+++ src/main/java/com/codeashobby/leetcode/MinimumSizeSubarraySum.java
@@ -0,0 +1,35 @@
+package com.codeashobby.leetcode;
+
+/**
+ * Created by hzhou on 5/11/15. codeashobby@gmail.com
+ * <p/>
+ * http://www.geeksforgeeks.org/minimum-length-subarray-sum-greater-given-value/
+ */
+public class MinimumSizeSubarraySum {
+
+	public int minSubArrayLen(int s, int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		int length = nums.length;
+		int start, end, crtSum, minLength;
+		start = end = crtSum = 0;
+		minLength = length + 1;
+
+		while (end < length) {
+
+			while (crtSum < s && end < length) {
+				crtSum += nums[end++];
+			}
+
+			while (crtSum >= s && start < length) {
+				if (end - start < minLength) {
+					minLength = end - start;
+				}
+				crtSum -= nums[start++];
+			}
+		}
+
+		return minLength > length ? 0 : minLength;
+	}
+}

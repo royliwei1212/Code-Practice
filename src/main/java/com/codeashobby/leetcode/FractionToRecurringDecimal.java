b514b0318bda9f352cc9eb03a0cf16acfb2a35c4
Hao Zhou <royhzhou@gmail.com>
2015-05-13 21:18:12 -0400

    add FractionToRecurringDecimal

--- src/main/java/com/codeashobby/leetcode/FractionToRecurringDecimal.java
+++ src/main/java/com/codeashobby/leetcode/FractionToRecurringDecimal.java
@@ -2,6 +2,9 @@ package com.codeashobby.leetcode;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by hzhou on 5/10/15. codeashobby@gmail.com
  * <p/>
@@ -18,25 +21,51 @@ import org.junit.Test;
  * Given numerator = 2, denominator = 3, return "0.(6)".
  */
 public class FractionToRecurringDecimal {
+    //TODO
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (denominator == 0) {
+            return "";
+        }
 
-	public String fractionToDecimal(int numerator, int denominator) {
-		if (denominator == 0) {
-			return "";
-		}
-		int pos = 1;
-		if (numerator / denominator < 0) {
-			pos = -1;
-		}
-		long n = Math.abs(numerator);
-		long d = Math.abs(denominator);
-		StringBuffer sb = new StringBuffer();
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        if ((numerator < 0) ^ (denominator < 0)) {
+            sb.append("-");
+        }
+        // first assign, then get Math.abs()
+        long n = numerator;
+        long d = denominator;
+        n = Math.abs(n);
+        d = Math.abs(d);
+        sb.append(n / d);
 
+        long remain = n % d;
+        remain *= 10;
 
-		return null;
-	}
+        if (remain == 0) return sb.toString();
 
-	@Test
-	public void test() {
-		String result = fractionToDecimal(76, 7);
-	}
+        sb.append(".");
+        Map<Long, Integer> map = new HashMap<Long, Integer>();
+        while (remain != 0) {
+            if (map.containsKey(remain)) {
+                int start = map.get(remain);
+                sb.insert(start, "(");
+                sb.append(")");
+                break;
+            }
+            map.put(remain, sb.length());
+            sb.append(remain / d);
+            remain = (remain % d) * 10;
+        }
+
+        return sb.toString();
+    }
+
+    @Test
+    public void test() {
+        String result = fractionToDecimal(-1, -2147483648);
+        System.out.println(result);
+    }
 }

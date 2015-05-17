d76c9853e756d3cdc361882475f40199147b5aa1
Hao Zhou <royhzhou@gmail.com>
2015-05-17 15:18:15 -0400

    add ConvertSortedListToBST

---
+++ src/main/java/com/codeashobby/leetcode/ConvertSortedListToBST.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode;
+
+import com.codeashobby.leetcode.parent.ListNode;
+import com.codeashobby.leetcode.parent.TreeNode;
+
+/**
+ * Description: Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced
+ * BST.
+ *
+ * @author hzhou
+ *
+ * http://www.programcreek.com/2013/01/leetcode-convert-sorted-list-to-binary-search-tree-java/
+ */
+
+// TODO: Important
+public class ConvertSortedListToBST {
+
+	ListNode crtHead;
+
+	public TreeNode sortedListToBST(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+		crtHead = head;
+		int length = 0;
+		ListNode cursor = head;
+		while (cursor != null) {
+			length++;
+			cursor = cursor.next;
+		}
+
+		return helper(0, length - 1);
+
+	}
+
+	private TreeNode helper(int start, int end) {
+		if (start > end) {
+			return null;
+		}
+
+		int middle = (start + end) / 2;
+		TreeNode left = helper(start, middle - 1);
+		TreeNode root = new TreeNode(crtHead.val);
+		crtHead = crtHead.next;
+		TreeNode right = helper(middle + 1, end);
+		root.left = left;
+		root.right = right;
+		return root;
+	}
+}

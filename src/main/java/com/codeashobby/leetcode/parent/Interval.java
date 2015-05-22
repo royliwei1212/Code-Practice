04cebdaf02e37ec3cdd8c1950417a4da7fe93282
Hao Zhou <hzhou.me@gmail.com>
2015-05-21 21:50:52 -0400

    add Merge Intervals

---
+++ src/main/java/com/codeashobby/leetcode/parent/Interval.java
@@ -0,0 +1,20 @@
+package com.codeashobby.leetcode.parent;
+
+/**
+ * Created by hzhou on 2015/5/21.
+ * Email: codeashobby@gmail.com
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}

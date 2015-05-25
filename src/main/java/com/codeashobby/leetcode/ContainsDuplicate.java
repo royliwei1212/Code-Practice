0018bd087fe63c59e378fff062878d2502538697
Hao Zhou <royhzhou@gmail.com>
2015-05-25 00:53:00 -0400

    add Contains Duplicate

---
+++ src/main/java/com/codeashobby/leetcode/ContainsDuplicate.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Description: Given an array of integers, find if the array contains any duplicates. Your function should return true
+ * if any value appears at least twice in the array, and it should return false if every element is distinct.
+ *
+ * @author hzhou
+ */
+public class ContainsDuplicate {
+
+	public boolean containsDuplicate(int[] nums) {
+		if (nums == null || nums.length <= 0) {
+			return false;
+		}
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i : nums) {
+			if (set.contains(i)) {
+				return true;
+			} else {
+				set.add(i);
+			}
+		}
+		return false;
+	}
+}

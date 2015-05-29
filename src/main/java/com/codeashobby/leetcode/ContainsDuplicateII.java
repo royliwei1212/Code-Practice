53ffd2d713527da0e866e5be5ea5a11b63c4124d
Hao Zhou <royhzhou@gmail.com>
2015-05-29 00:17:05 -0400

    add Contains Duplicate II

---
+++ src/main/java/com/codeashobby/leetcode/ContainsDuplicateII.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description: Given an array of integers and an integer k, return true if and only if there are two distinct indices i
+ * and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+ *
+ * @author hzhou
+ */
+public class ContainsDuplicateII {
+
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+		if (nums == null || nums.length < 2 || k < 1) {
+			return false;
+		}
+
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if (!map.containsKey(nums[i])) {
+				map.put(nums[i], i);
+			} else {
+				if (map.get(nums[i]) < i - k) {
+					map.put(nums[i], i);
+				} else {
+					return true;
+				}
+			}
+
+		}
+		return false;
+	}
+}

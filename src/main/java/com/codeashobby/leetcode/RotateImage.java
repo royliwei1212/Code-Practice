f5fc804b4625569ee5564645f5259bc988946ab0
Hao Zhou <royhzhou@gmail.com>
2015-05-29 18:04:09 -0400

    add Rotate Image

---
+++ src/main/java/com/codeashobby/leetcode/RotateImage.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode;
+
+/**
+ * Description:
+ *
+ * @author hzhou
+ */
+public class RotateImage {
+
+	public void rotate(int[][] matrix) {
+		if (matrix == null || matrix.length < 2) {
+			return;
+		}
+		int n = matrix.length;
+		int m = n;
+		if (n % 2 != 0) {
+			m++;
+		}
+		for (int i = 0; i < n / 2; i++) {
+			for (int j = 0; j < m / 2; j++) {
+				int tmp = matrix[i][j];
+				matrix[i][j] = matrix[n - 1 - j][i];
+				matrix[n - 1 - j][i] = matrix[n - i - 1][n - 1 - j];
+				matrix[n - i - 1][n - 1 - j] = matrix[j][n - 1 - i];
+				matrix[j][n - 1 - i] = tmp;
+			}
+		}
+	}
+}

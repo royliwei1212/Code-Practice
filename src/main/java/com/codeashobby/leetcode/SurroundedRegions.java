c257d9bcf4e0dfc98893242fd4afdac695a4266b
Hao Zhou <royhzhou@gmail.com>
2015-05-29 17:24:37 -0400

    add SurroundedRegions

--- src/main/java/com/codeashobby/leetcode/SurroundedRegions.java
+++ src/main/java/com/codeashobby/leetcode/SurroundedRegions.java
@@ -3,6 +3,9 @@
  */
 package com.codeashobby.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Description:
  *
@@ -10,25 +13,40 @@ package com.codeashobby.leetcode;
  */
 public class SurroundedRegions {
 
+	Queue<Pair> queue = new LinkedList<Pair>();
+
 	public void solve(char[][] board) {
 		if (board == null || board.length < 3 || board[0] == null || board[0].length < 3) {
 			return;
 		}
 
 		for (int i = 0; i < board.length; i++) {
-			helper(board, i, 0);
+			if (board[i][0] == 'O') {
+				helper(board, i, 0);
+			}
+
+			if (board[i][board[0].length - 1] == 'O') {
+				helper(board, i, board[0].length - 1);
+			}
 		}
 
 		for (int i = 0; i < board[0].length; i++) {
-			helper(board, 0, i);
+			if (board[0][i] == 'O') {
+				helper(board, 0, i);
+			}
+
+			if (board[board.length - 1][i] == 'O') {
+				helper(board, board.length - 1, i);
+			}
+
 		}
 
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
-				if(board[i][j] == 'O') {
+				if (board[i][j] == 'O') {
 					board[i][j] = 'X';
 				}
-				if(board[i][j] == '#') {
+				if (board[i][j] == '#') {
 					board[i][j] = 'O';
 				}
 			}
@@ -36,17 +54,33 @@ public class SurroundedRegions {
 	}
 
 	private void helper(char[][] board, int i, int j) {
-		if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+
+		search(board, i, j);
+
+		while (!queue.isEmpty()) {
+			Pair p = queue.poll();
+			search(board, p.x + 1, p.y);
+			search(board, p.x - 1, p.y);
+			search(board, p.x, p.y + 1);
+			search(board, p.x, p.y - 1);
+		}
+	}
+
+	private void search(char[][] board, int i, int j) {
+		if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
 			return;
 		}
+		queue.add(new Pair(i, j));
+		board[i][j] = '#';
+	}
 
-		char c = board[i][j];
-		if (c == 'O') {
-			board[i][j] = '#';
-			helper(board, i + 1, j);
-			helper(board, i - 1, j);
-			helper(board, i, j + 1);
-			helper(board, i, j - 1);
+	static class Pair {
+
+		Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
 		}
+
+		int x, y;
 	}
 }

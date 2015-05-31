9eb136667ec0bcae36303bab78cdcacad99e8f08
Hao Zhou <royhzhou@gmail.com>
2015-05-30 21:36:45 -0400

    add Jump Game II

--- src/main/java/com/codeashobby/leetcode/JumpGameII.java
+++ src/main/java/com/codeashobby/leetcode/JumpGameII.java
@@ -16,6 +16,8 @@ import org.junit.Test;
  * <p/>
  * The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last
  * index.)
+ * <p/>
+ * http://www.lifeincode.net/programming/leetcode-jump-game-and-jump-game-ii-java/
  *
  * @author hzhou
  */
@@ -23,26 +25,31 @@ public class JumpGameII {
 
 	public int jump(int[] nums) {
 
-		if (nums == null || nums.length < 1) {
-			return nums == null ? 0 : nums.length;
-		}
-		int[] map = new int[nums.length];
-		for (int i = 0; i < map.length; i++) {
-			map[i] = Integer.MAX_VALUE;
-		}
-		map[0] = 0;
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j <= i + nums[i] && j < map.length; j++) {
-				map[j] = Math.min(map[j], map[i] + 1);
-			}
+		if (nums == null || nums.length <= 1) {
+			return 0;
 		}
 
-		return map[nums.length - 1];
+		int jump = 1;
+		int step = nums[0];
+		int maxReach = nums[0];
+
+		for (int i = 1; i < nums.length-1; i++) {
+
+			maxReach = Math.max(maxReach, i + nums[i]);
+			step--;
+			if (step == 0) {
+				step = maxReach - i;
+				jump++;
+			}
+
+		}
+
+		return jump;
 	}
 
 	@Test
 	public void test() {
-		int[] nums = new int[]{2, 3, 1, 1, 4};
+		int[] nums = new int[]{1,3,2};
 		int result = jump(nums);
 	}
 }

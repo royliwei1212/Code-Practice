a79d8bcbd0a3240d70a91ac61ade1806997a0959
Hao Zhou <royhzhou@gmail.com>
2015-05-31 22:49:45 -0400

    add Maximal Rectangle

--- src/main/java/com/codeashobby/leetcode/MaximalRectangle.java
+++ src/main/java/com/codeashobby/leetcode/MaximalRectangle.java
@@ -1,77 +1,54 @@
 package com.codeashobby.leetcode;
 
+import java.util.Stack;
+
 /**
  * Created by hzhou on 4/22/15. codeashobby@gmail.com
  */
 public class MaximalRectangle {
 
 	public int maximalRectangle(char[][] matrix) {
-		int height = matrix.length;
-		if (height == 0) {
-			return 0;
-		}
-		int width = matrix[0].length;
-
-		int ml = -1;
-		int mt = -1;
-		int mr = width;
-		int mb = height;
-
-		for (int i = 0; i < width; i++) {
-			if (checkRow(i, matrix)) {
-				ml = i;
-				break;
-			}
-		}
-		// if no 1 found
-		if (ml == -1) {
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
 			return 0;
 		}
 
-		for (int i = width - 1; i >= ml; i--) {
-			if (checkRow(i, matrix)) {
-				mr = i;
-				break;
-			}
-		}
-
-		for (int i = 0; i < height; i++) {
-			if (checkLine(i, matrix)) {
-				mt = i;
-				break;
-			}
-		}
-
-		for (int i = height - 1; i >= mt; i--) {
-			if (checkLine(i, matrix)) {
-				mb = i;
-				break;
-			}
-		}
-
-		return (mr - ml + 1) * (mb - mt + 1);
-
-	}
-
-	private boolean checkLine(int line, char[][] matrix) {
-		int sum = 0;
-		for (int i = 0; i < matrix[line].length; i++) {
-			sum += matrix[line][i] - '0';
-			if (sum > 0) {
-				return true;
-			}
-		}
-		return sum > 0;
-	}
-
-	private boolean checkRow(int row, char[][] matrix) {
-		int sum = 0;
+		int[][] map = new int[matrix.length][matrix[0].length + 1];
 		for (int i = 0; i < matrix.length; i++) {
-			sum += matrix[i][row] - '0';
-			if (sum > 0) {
-				return true;
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] == '0') {
+					map[i][j] = 0;
+				} else {
+					map[i][j] = i == 0 ? 1 : map[i - 1][j] + 1;
+				}
 			}
 		}
-		return sum > 0;
+
+		int max = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			int value = helper(map[i]);
+			max = Math.max(max, value);
+		}
+
+		return max;
+	}
+
+	public int helper(int[] height) {
+		if (height == null || height.length == 0) {
+			return 0;
+		}
+		Stack<Integer> stack = new Stack<Integer>();
+
+		int crt = 0;
+		int max = 0;
+		while (crt < height.length) {
+			if (stack.isEmpty() || height[crt] >= height[stack.peek()]) {
+				stack.push(crt++);
+			} else {
+				int i = stack.pop();
+				max = Math.max(max, height[i] * (stack.isEmpty() ? crt : crt - stack.peek() - 1));
+			}
+		}
+
+		return max;
 	}
 }

50ce4a80bebf526bc99b913e677f78b805cffa82
Hao Zhou <i@hzhou.me>
2015-06-07 18:27:20 -0400

    update ContainsDuplicateIII

--- src/main/java/com/codeashobby/leetcode/ContainsDuplicateIII.java
+++ src/main/java/com/codeashobby/leetcode/ContainsDuplicateIII.java
@@ -3,71 +3,43 @@
  */
 package com.codeashobby.leetcode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import org.junit.Test;
+
+import java.util.TreeSet;
 
 /**
  * Description:
+ * <p>
+ * http://bookshadow.com/weblog/2015/06/03/leetcode-contains-duplicate-iii/
  *
  * @author hzhou
  */
 
-// TODO: not finished
 public class ContainsDuplicateIII {
 
-	public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-		if (nums == null || nums.length < 2 || k < 1) {
-			return false;
-		}
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (k < 1 || t < 0) {
+            return false;
+        }
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            if (set.floor(n) != null && n <= t + set.floor(n) ||
+                    set.ceiling(n) != null && set.ceiling(n) <= t + n) {
+                return true;
+            }
+            set.add(n);
+            if (i >= k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
 
-		List<Item> list = new ArrayList<Item>();
-		for (int i = 0; i < nums.length; i++) {
-			list.add(new Item(i, nums[i]));
-		}
-		Collections.sort(list, new Comparator<Item>() {
-			public int compare(Item o1, Item o2) {
-				return o1.val - o2.val;
-			}
-		});
 
-		for (int i = 0; i < list.size(); i++) {
-			Item item = list.get(i);
-			for (int j = i + 1; j <= i + k && j < list.size(); j++) {
-				Item item2 = list.get(j);
-				if (item2.index == item.index) {
-					continue;
-				}
-				if (Math.abs(item.val - item2.val) > t) {
-					break;
-				}
-
-				if (Math.abs(item2.index - item.index) <= k && Math.abs(item.val - item2.val) <= t) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	static class Item {
-
-		int index;
-		int val;
-
-		Item(int index, int val) {
-			this.index = index;
-			this.val = val;
-		}
-	}
-
-	@Test
-	public void test() {
-		int[] nums = new int[]{1, 3, 6, 2};
-		boolean b = containsNearbyAlmostDuplicate(nums, 1, 2);
-	}
+    @Test
+    public void test() {
+        int[] nums = new int[]{1, 3, 6, 2};
+        boolean b = containsNearbyAlmostDuplicate(nums, 1, 2);
+    }
 }

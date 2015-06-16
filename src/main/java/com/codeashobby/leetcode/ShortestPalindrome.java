08c43b1989035175b21a11d3545a66ed73a5016c
Hao Zhou <i@hzhou.me>
2015-06-15 23:37:22 -0400

    add ShortestPalindrome

---
+++ src/main/java/com/codeashobby/leetcode/ShortestPalindrome.java
@@ -0,0 +1,23 @@
+package com.codeashobby.leetcode;
+
+/**
+ * Created by hzhou on 2015/6/15.
+ * Email: i@hzhou.me
+ */
+public class ShortestPalindrome {
+    public String shortestPalindrome(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        String r = new StringBuilder(s).reverse().toString();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.startsWith(r.substring(i))) {
+                return r.substring(0, i) + s;
+            }
+        }
+
+        return s;
+    }
+}

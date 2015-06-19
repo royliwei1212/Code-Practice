b76271e1ec54a8b32a2f79a4ef3bc35a7faade91
Hao Zhou <i@hzhou.me>
2015-06-18 23:29:48 -0400

    update CompareVersionNumbers

--- src/main/java/com/codeashobby/leetcode/CompareVersionNumbers.java
+++ src/main/java/com/codeashobby/leetcode/CompareVersionNumbers.java
@@ -8,54 +8,42 @@ import org.junit.Test;
  */
 public class CompareVersionNumbers {
 
-	private static final Logger log = Logger.getLogger(CompareVersionNumbers.class);
+    private static final Logger log = Logger.getLogger(CompareVersionNumbers.class);
 
-	public int compareVersion(String version1, String version2) {
-		assert version1 != null;
-		assert version2 != null;
+    public int compareVersion(String version1, String version2) {
+        assert version1 != null;
+        assert version2 != null;
 
-		String[] splits1 = version1.split("\\.");
-		String[] splits2 = version2.split("\\.");
-		int len1 = splits1.length;
-		int len2 = splits2.length;
-		int result = 0;
-		for (int i = 0; i < Math.min(len1, len2); i++) {
-			int val1 = Integer.valueOf(splits1[i]);
-			int val2 = Integer.valueOf(splits2[i]);
-			if (val1 > val2) {
-				result = 1;
-				break;
-			} else if (val1 < val2) {
-				result = -1;
-				break;
-			}
-		}
+        version1 = version1.trim();
+        version2 = version2.trim();
 
-		if (result == 0 && len1 != len2) {
-			if (len1 > len2) {
-				for (int i = len2; i < len1; i++) {
-					if (Integer.valueOf(splits1[i]) != 0) {
-						result = 1;
-						break;
-					}
-				}
-			} else {
-				for (int i = len1; i < len2; i++) {
-					if (Integer.valueOf(splits2[i]) != 0) {
-						result = -1;
-						break;
-					}
-				}
-			}
-		}
-		return result;
-	}
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
 
-	@Test
-	public void test() {
-		String[] strs = "22.33".split("\\.");
-		for (String str : strs) {
-			log.info(str);
-		}
-	}
+        int result = 0;
+        int l1 = v1.length;
+        int l2 = v2.length;
+
+        for (int i = 0; i < Math.max(l1, l2); i++) {
+            int a = ((l1 <= i) || v1[i].isEmpty()) ? 0 : Integer.valueOf(v1[i]);
+            int b = ((l2 <= i) || v2[i].isEmpty()) ? 0 : Integer.valueOf(v2[i]);
+            if (a > b) {
+                result = 1;
+                break;
+            } else if (a < b) {
+                result = -1;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    @Test
+    public void test() {
+        String[] strs = "22.33".split("\\.");
+        for (String str : strs) {
+            log.info(str);
+        }
+    }
 }

c1ffd333e01f3969f87262b090fac8282627dffa
Hao Zhou <codeashobby@gmail.com>
2015-06-28 22:25:11 -0400

    update TwoSum

--- src/main/java/com/codeashobby/leetcode/TwoSum.java
+++ src/main/java/com/codeashobby/leetcode/TwoSum.java
@@ -9,31 +9,17 @@ import java.util.Map;
 public class TwoSum {
 
 	public int[] twoSum(int[] nums, int target) {
-		assert nums.length > 0;
-
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		int[] result = new int[2];
-
-		for (int i = 0; i < nums.length; i++) {
-			if (map.containsKey(nums[i]) && 2 * nums[i] == target) {
-				result[0] = map.get(nums[i]) + 1;
-				result[1] = i + 1;
+		for(int i = 0; i < nums.length;i++){
+			int x = target - nums[i];
+			if(map.containsKey(x)) {
+				result[0] = map.get(x);
+				result[1] = i+1;
 				return result;
-			} else {
-				map.put(nums[i], i);
 			}
+			map.put(nums[i], i+1);
 		}
-
-		for (int i = 0; i < nums.length; i++) {
-			int left = target - nums[i];
-			if (map.containsKey(left) && i != map.get(left)) {
-				result[0] = i + 1;
-				result[1] = map.get(left) + 1;
-				break;
-			}
-		}
-
 		return result;
-
 	}
 }

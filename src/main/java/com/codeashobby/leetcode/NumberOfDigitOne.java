0866ff5ea86da6870b09a88afb29cd8b3d8e416e
Hao Zhou <royhzhou@gmail.com>
2015-07-31 18:14:45 -0400

    add draft

---
+++ src/main/java/com/codeashobby/leetcode/NumberOfDigitOne.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2015 Ovitas Inc, All rights reserved.
+ */
+package com.codeashobby.leetcode;
+
+/**
+ * Description: Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than
+ * or equal to n.
+ * <pre>
+ * For example:
+ * Given n = 13,
+ * Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+ * </pre>
+ *
+ * @author hzhou
+ */
+public class NumberOfDigitOne {
+
+	public int countDigitOne(int n) {
+		//todo
+		return 1;
+	}
+}

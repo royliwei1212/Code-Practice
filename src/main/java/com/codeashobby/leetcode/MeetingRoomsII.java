0777131252974e88a2e3eb96e025a93694d00f39
Hao Zhou <i@hzhou.me>
2015-08-07 21:54:50 -0400

    add MeetingRoomsII

--- src/main/java/com/codeashobby/leetcode/MeetingRoomsII.java
+++ src/main/java/com/codeashobby/leetcode/MeetingRoomsII.java
@@ -4,6 +4,7 @@
 package com.codeashobby.leetcode;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.TreeSet;
 
 import com.codeashobby.leetcode.parent.Interval;
@@ -16,40 +17,35 @@ import org.junit.Test;
  */
 public class MeetingRoomsII {
 
-	public int minMeetingRooms(Interval[] intervals) {
-		if (intervals == null || intervals.length == 0) {
-			return 0;
-		}
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
 
-		Arrays.sort(intervals, (o1, o2) -> {
-			int r = o1.start - o2.start;
-			return r == 0 ? o1.end - o2.end : r;
-		});
+        Arrays.sort(intervals, (o1, o2) -> {
+            int r = o1.start - o2.start;
+            return r == 0 ? o1.end - o2.end : r;
+        });
 
-		TreeSet<Interval> treeSet = new TreeSet<>((o1, o2) -> o1.end - o2.end);
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
-		treeSet.add(intervals[0]);
+        queue.add(intervals[0].end);
 
-		for (int i = 1; i < intervals.length; i++) {
-			Interval val = treeSet.first();
-			Interval in = intervals[i];
-			if (in.start >= val.end) {
-				treeSet.remove(val);
-			}
-			treeSet.add(in);
-		}
+        for (int i = 1; i < intervals.length; i++) {
+            int val = queue.peek();
+            Interval in = intervals[i];
+            if (in.start >= val) {
+                queue.remove(val);
+            }
+            queue.add(in.end);
+        }
+        return queue.size();
+    }
 
-		return treeSet.size();
-	}
+    @Test
+    public void test() {
+        Interval[] intervals = new Interval[]{new Interval(5, 8), new Interval(6, 8)};
+        System.out.println(minMeetingRooms(intervals));
 
-	@Test
-	public void test() {
-		Interval[] intervals = new Interval[]{new Interval(5, 8), new Interval(6, 8)};
-		//System.out.println(minMeetingRooms(intervals));
-
-		TreeSet<Interval> treeSet = new TreeSet<>();
-		for(Interval i : intervals) {
-			treeSet.add(i);
-		}
-	}
+    }
 }

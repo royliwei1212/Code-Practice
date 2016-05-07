7810e4a9a0c88e282e51934d11693c89b823d2e1
Hao Zhou <i@hzhou.me>
2016-05-07 00:00:19 -0400

    finish ReverseVowelsOfaString

--- src/main/java/com/codeashobby/leetcode/ReverseVowelsOfaString.java
+++ src/main/java/com/codeashobby/leetcode/ReverseVowelsOfaString.java
@@ -9,9 +9,56 @@ import java.util.Set;
  */
 public class ReverseVowelsOfaString {
 
-    // TODO
-    private Set<Character> vowels = new HashSet<>();
+    private Set<Character> vowels = new HashSet<Character>() {{
+        add('A');
+        add('a');
+        add('E');
+        add('e');
+        add('I');
+        add('i');
+        add('O');
+        add('o');
+        add('U');
+        add('u');
+
+    }};
+
     public String reverseVowels(String s) {
-        return null;
+
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            while (l < r) {
+                if (vowels.contains(chars[l])) {
+                    break;
+                } else {
+                    l++;
+                }
+            }
+            while (l < r) {
+                if (vowels.contains(chars[r])) {
+                    break;
+                } else {
+                    r--;
+                }
+            }
+
+            if (l < r) {
+                char tmp = chars[l];
+                chars[l] = chars[r];
+                chars[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c : chars){
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }

4d31133588ffbc2ccccb63fa6e39329f3090372f
Hao Zhou <i@hzhou.me>
2016-05-06 23:32:43 -0400

    add ReverseVowelsOfaString draft

---
+++ src/main/java/com/codeashobby/leetcode/ReverseVowelsOfaString.java
@@ -0,0 +1,17 @@
+package com.codeashobby.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by hzhou on 2016/5/6.
+ * Email: i@hzhou.me
+ */
+public class ReverseVowelsOfaString {
+
+    // TODO
+    private Set<Character> vowels = new HashSet<>();
+    public String reverseVowels(String s) {
+        return null;
+    }
+}

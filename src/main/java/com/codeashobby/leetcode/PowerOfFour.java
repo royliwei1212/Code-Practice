9454af1db3e1cd16fa98d268cd74f2dc74f1d993
Hao Zhou <i@hzhou.me>
2016-05-11 00:11:28 -0400

    add PowerOfFour

---
+++ src/main/java/com/codeashobby/leetcode/PowerOfFour.java
@@ -0,0 +1,11 @@
+package com.codeashobby.leetcode;
+
+/**
+ * Created by hzhou on 2016/5/11.
+ * Email: i@hzhou.me
+ */
+public class PowerOfFour {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (0x55555555 & num) != 0;
+    }
+}

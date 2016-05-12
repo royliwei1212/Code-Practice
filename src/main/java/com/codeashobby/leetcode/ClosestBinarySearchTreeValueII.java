5e75a35e89f60890c613da86d9f1d6a2c320906d
Hao Zhou <i@hzhou.me>
2016-05-11 23:14:20 -0400

    add ClosestBinarySearchTreeValueII

---
+++ src/main/java/com/codeashobby/leetcode/ClosestBinarySearchTreeValueII.java
@@ -0,0 +1,52 @@
+package com.codeashobby.leetcode;
+
+import com.codeashobby.leetcode.parent.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+/**
+ * Created by hzhou on 2016/5/11.
+ * Email: i@hzhou.me
+ * <p>
+ * https://segmentfault.com/a/1190000003797291
+ */
+public class ClosestBinarySearchTreeValueII {
+
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        Queue<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (result.size() < k) {
+                result.offer(node.val);
+            } else {
+                int first = result.peek();
+                if (Math.abs(first - target) > Math.abs(node.val - target)) {
+                    result.poll();
+                    result.offer(node.val);
+                } else {
+                    break;
+                }
+            }
+
+            if (node.right != null) {
+                TreeNode right = node.right;
+                while (right != null) {
+                    stack.add(right);
+                    right = right.left;
+                }
+            }
+        }
+
+        return result.stream().collect(Collectors.toList());
+    }
+}

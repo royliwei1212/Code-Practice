addf872c8c229e8049d14af3d281129ae42d2fb8
Hao Zhou <i@hzhou.me>
2016-05-14 22:48:46 -0400

    add NimGame

---
+++ src/main/java/com/codeashobby/leetcode/NimGame.java
@@ -0,0 +1,11 @@
+package com.codeashobby.leetcode;
+
+/**
+ * Created by hzhou on 2016/5/14.
+ * Email: i@hzhou.me
+ */
+public class NimGame {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}

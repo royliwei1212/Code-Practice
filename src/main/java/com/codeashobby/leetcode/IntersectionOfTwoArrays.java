f384c12191c53ce3d3a4b3d3b7af08ce9965bfce
Hao Zhou <i@hzhou.me>
2016-05-18 00:40:41 -0400

    add IntersectionOfTwoArrays

---
+++ src/main/java/com/codeashobby/leetcode/IntersectionOfTwoArrays.java
@@ -0,0 +1,34 @@
+package com.codeashobby.leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by hzhou on 2016/5/18.
+ * Email: i@hzhou.me
+ */
+public class IntersectionOfTwoArrays {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+        Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> result = new HashSet<>();
+
+        for (int n : nums2) {
+            if (set1.contains(n)) {
+                result.add(n);
+            }
+        }
+        int[] res = new int[result.size()];
+        int i = 0;
+        for (int x : result) {
+            res[i++] = x;
+        }
+        return res;
+    }
+
+}

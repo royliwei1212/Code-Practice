e00428123028f8950db69ef370816183ea4beb89
Hao Zhou <i@hzhou.me>
2016-05-28 15:42:50 -0400

    update SparseMatrixMultiplication

--- src/main/java/com/codeashobby/leetcode/SparseMatrixMultiplication.java
+++ src/main/java/com/codeashobby/leetcode/SparseMatrixMultiplication.java
@@ -11,27 +11,16 @@ public class SparseMatrixMultiplication {
             return new int[0][0];
         }
 
-        int[][] result = new int[A.length][B.length];
+        int[][] result = new int[A.length][B[0].length];
         int length = B[0].length;
-        for (int i = 0; i < length; i++) {
-            for (int k = 0; k < A.length; k++) {
-                result[k][i] = getSum(A, k, B, i);
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] == 0) continue;
+                for (int k = 0; k < length; k++) {
+                    result[i][k] += A[i][j] * B[j][k];
+                }
             }
         }
         return result;
     }
-
-    private int getSum(int[][] A, int a, int[][] B, int b) {
-        int sum = 0;
-
-        for (int i = 0; i < A[0].length; i++) {
-            int x = A[a][i];
-            int y = B[i][b];
-            sum += x * y;
-        }
-
-        return sum;
-    }
-
-
 }

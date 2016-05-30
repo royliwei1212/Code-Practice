4b8b8ea5879f5e391d203aec6f710b2019f4572a
Hao Zhou <i@hzhou.me>
2016-05-30 17:30:15 -0400

    add BulbSwitcher

---
+++ src/main/java/com/codeashobby/leetcode/BulbSwitcher.java
@@ -0,0 +1,13 @@
+package com.codeashobby.leetcode;
+
+/**
+ * Created by hzhou on 2016/5/28.
+ * Email: i@hzhou.me
+ */
+public class BulbSwitcher {
+
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+
+}

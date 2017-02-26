39c9576e2f3d1ebc58786e018fe8396b5db51cb0
zhouhao <i@hzhou.me>
2017-02-26 14:52:41 -0500

    add Detect Capital

---
+++ src/main/java/com/codeashobby/leetcode/DetectCapital.java
@@ -0,0 +1,16 @@
+package com.codeashobby.leetcode;
+
+/**
+ * @author hzhou
+ * @since 2/26/17
+ */
+public class DetectCapital {
+
+    public boolean detectCapitalUse(String word) {
+        if (word == null || word.isEmpty()) {
+            return false;
+        }
+
+        return word.equals(word.toUpperCase()) || word.substring(1).equals(word.substring(1).toLowerCase());
+    }
+}

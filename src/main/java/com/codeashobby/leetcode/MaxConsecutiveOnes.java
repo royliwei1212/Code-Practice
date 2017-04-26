f7ed84a0e16c76ae490b938417cd62886df32452
Hao Zhou <i@hzhou.me>
2017-04-25 23:11:06 -0400

    add MaxConsecutiveOnes

---
+++ src/main/java/com/codeashobby/leetcode/MaxConsecutiveOnes.java
@@ -0,0 +1,25 @@
+package com.codeashobby.leetcode;
+
+/**
+ * @author hzhou
+ * @since 4/25/17
+ */
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int max = 0;
+        int crt = 0;
+        for (int i : nums) {
+            if (i == 0) {
+                max = Math.max(max, crt);
+                crt = 0;
+            } else {
+                crt++;
+            }
+        }
+        return Math.max(max, crt);
+    }
+}

e1587fd0a74e0a7a14dc2f99b9f1ec06eeeb3041
Hao Zhou <i@hzhou.me>
2017-07-27 13:43:02 -0400

    add Split Array with Equal Sum

---
+++ src/main/java/tech/saltyegg/leetcode/SplitArrayWithEqualSum.java
@@ -0,0 +1,36 @@
+package tech.saltyegg.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SplitArrayWithEqualSum {
+
+    public boolean splitArray(int[] nums) {
+
+        if (nums == null || nums.length < 7) {
+            return false;
+        }
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] += sum[i - 1] + nums[i];
+        }
+
+
+        for (int j = 3; j < nums.length - 3; j++) {
+            Set<Integer> set = new HashSet<>();
+            for (int i = 1; i < j - 1; i++) {
+                if (sum[i - 1] == sum[j - 1] - sum[i]) {
+                    set.add(sum[i - 1]);
+                }
+            }
+            for (int k = j + 2; k < nums.length - 1; k++) {
+                if (sum[k - 1] - sum[j] == sum[nums.length - 1] - sum[k] && set.contains(sum[k - 1] - sum[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}

45af89044085eec65179ac69e0339bf7529a862b
Hao Zhou <i@hzhou.me>
2017-07-27 14:36:23 -0400

    Time Limit Exceeded

--- src/main/java/tech/saltyegg/leetcode/GuessNumberHigherOrLower.java
+++ src/main/java/tech/saltyegg/leetcode/GuessNumberHigherOrLower.java
@@ -5,22 +5,22 @@ import tech.saltyegg.leetcode.parent.GuessGame;
 public class GuessNumberHigherOrLower extends GuessGame {
 
     public int guessNumber(int n) {
-        return helper(1, n);
+        int l = 1;
+        int r = n;
+        while (l < r) {
+            int m = (l + r) / 2;
+            int res = guess(m);
+            if (res == 0) {
+                return m;
+            }
+
+            if (res < 0) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
     }
 
-    private int helper(int l, int r) {
-        if (l == r) return l;
-
-        int m = (l + r) / 2;
-        int res = guess(m);
-        if (res == 0) {
-            return m;
-        }
-
-        if (res < 0) {
-            return helper(l, m - 1);
-        } else {
-            return helper(m + 1, r);
-        }
-    }
 }

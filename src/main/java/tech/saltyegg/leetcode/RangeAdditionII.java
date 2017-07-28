f05484b452f00a3ded495659d3c49bfdd412be47
Hao Zhou <i@hzhou.me>
2017-07-28 15:52:18 -0400

    add Range Addition II

---
+++ src/main/java/tech/saltyegg/leetcode/RangeAdditionII.java
@@ -0,0 +1,18 @@
+package tech.saltyegg.leetcode;
+
+public class RangeAdditionII {
+
+    public int maxCount(int m, int n, int[][] ops) {
+        if (ops.length == 0) {
+            return m * n;
+        }
+        int x = Integer.MAX_VALUE;
+        int y = Integer.MAX_VALUE;
+
+        for (int[] op : ops) {
+            x = Math.min(x, op[0]);
+            y = Math.min(y, op[1]);
+        }
+        return x * y;
+    }
+}

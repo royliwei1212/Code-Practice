7c83bdb79095b2c8c82a831e848cf2cc7dfe5fb5
Hao Zhou <i@hzhou.me>
2017-07-28 15:21:27 -0400

    add Average of Levels in Binary Tree

---
+++ src/main/java/tech/saltyegg/leetcode/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,43 @@
+package tech.saltyegg.leetcode;
+
+import tech.saltyegg.leetcode.parent.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class AverageOfLevelsInBinaryTree {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        long sum = 0;
+        int count = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                if (count != 0) {
+                    result.add(1.0 * sum / count);
+                }
+                sum = count = 0;
+                queue.add(null);
+                continue;
+            }
+            count++;
+            sum += node.val;
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return result;
+    }
+}

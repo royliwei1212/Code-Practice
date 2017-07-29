18249bb1dcfefc62715101e58a5362bc87b7a4d0
Hao Zhou <i@hzhou.me>
2017-07-29 18:29:47 -0400

    add Subtree of Another Tree

---
+++ src/main/java/tech/saltyegg/leetcode/SubtreeOfAnotherTree.java
@@ -0,0 +1,30 @@
+package tech.saltyegg.leetcode;
+
+import tech.saltyegg.leetcode.parent.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SubtreeOfAnotherTree {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(s);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (isSame(node, t)) return true;
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+        return false;
+    }
+
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null || s.val != t.val) {
+            return false;
+        }
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}

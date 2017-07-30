c9c510785220ef4efd258673877e90773d201757
Hao Zhou <i@hzhou.me>
2017-07-29 21:01:40 -0400

    add Diameter of Binary Tree

---
+++ src/main/java/tech/saltyegg/leetcode/DiameterOfBinaryTree.java
@@ -0,0 +1,31 @@
+package tech.saltyegg.leetcode;
+
+import tech.saltyegg.leetcode.parent.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DiameterOfBinaryTree {
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int result = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            result = Math.max(result, getMaxDepth(node.left) + getMaxDepth(node.right));
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+        return result;
+    }
+
+    private int getMaxDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(getMaxDepth(node.left), getMaxDepth(node.right)) + 1;
+    }
+}

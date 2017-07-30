4c6816bf2388b068de3733fae4d887326fbed150
Hao Zhou <i@hzhou.me>
2017-07-29 20:48:57 -0400

    add Minimum Absolute Difference in BST

---
+++ src/main/java/tech/saltyegg/leetcode/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,28 @@
+package tech.saltyegg.leetcode;
+
+import tech.saltyegg.leetcode.parent.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinimumAbsoluteDifferenceInBST {
+
+    public int getMinimumDifference(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        int result = Integer.MAX_VALUE;
+        for (int i = 1; i < list.size(); i++) {
+            result = Math.min(result, list.get(i) - list.get(i - 1));
+        }
+        return result;
+    }
+
+    private void helper(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        helper(node.left, list);
+        list.add(node.val);
+        helper(node.right, list);
+    }
+}

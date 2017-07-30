06b951e641d5519c73315f01bc77b9050a5353ad
Hao Zhou <i@hzhou.me>
2017-07-29 22:50:08 -0400

    still Time Limit Exceeded

--- src/main/java/tech/saltyegg/leetcode/RepeatedSubstringPattern.java
+++ src/main/java/tech/saltyegg/leetcode/RepeatedSubstringPattern.java
@@ -6,14 +6,10 @@ public class RepeatedSubstringPattern {
         if (s == null || s.length() < 2) {
             return false;
         }
-
-        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= s.length() / 2; i++) {
-            sb.setLength(0);
             String sub = s.substring(0, i);
-            if (sb.append(s.substring(i, s.length())).append(sub).toString().equals(s)) return true;
+            if ((s.substring(i, s.length()) + sub).equals(s)) return true;
         }
-
         return false;
     }
 

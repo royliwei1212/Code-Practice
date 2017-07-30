75ab8973628cc460c3fe7c03a17692c371d743e9
Hao Zhou <i@hzhou.me>
2017-07-30 15:14:33 -0400

    add Number of Segments in a String

---
+++ src/main/java/tech/saltyegg/leetcode/NumberOfSegmentsInAString.java
@@ -0,0 +1,12 @@
+package tech.saltyegg.leetcode;
+
+public class NumberOfSegmentsInAString {
+
+    public int countSegments(String s) {
+        if (s == null || s.trim().isEmpty()) {
+            return 0;
+        }
+        return s.trim().split("( )+").length;
+    }
+
+}

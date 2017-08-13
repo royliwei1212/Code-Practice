5fed2c44cd9da178f427f71bf47fd94c06ba9fee
Hao Zhou <i@hzhou.me>
2017-08-12 21:35:19 -0400

    add Two Sum IV - Input is a BST

---
+++ src/main/java/tech/saltyegg/leetcode/TwoSumIV.java
@@ -0,0 +1,38 @@
+package tech.saltyegg.leetcode;
+
+import tech.saltyegg.leetcode.parent.TreeNode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * @author hzhou
+ * @since 8/12/17
+ */
+public class TwoSumIV {
+
+    public boolean findTarget(TreeNode root, int k) {
+        Map<Integer, Integer> dict = new HashMap<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            dict.put(node.val, dict.getOrDefault(node.val, 0) + 1);
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : dict.entrySet()) {
+            int val = entry.getKey();
+            int tar = k - val;
+            if (val == tar && entry.getValue() > 1) return true;
+            if (val != tar && dict.containsKey(tar)) return true;
+
+        }
+        return false;
+    }
+}

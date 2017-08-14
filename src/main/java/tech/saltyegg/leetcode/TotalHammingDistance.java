338f9483e8bc2dd7949ed0470df965c0c18a4678
Hao Zhou <i@hzhou.me>
2017-08-13 23:00:32 -0400

    add Total Hamming Distance

--- src/main/java/tech/saltyegg/leetcode/TotalHammingDistance.java
+++ src/main/java/tech/saltyegg/leetcode/TotalHammingDistance.java
@@ -6,20 +6,12 @@ public class TotalHammingDistance {
         if (nums == null || nums.length < 2) return 0;
 
         int result = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                result += hammingDistance(nums[i], nums[j]);
-            }
-        }
-        return result;
-    }
-
-    private int hammingDistance(int a, int b) {
-        int result = 0;
         for (int i = 0; i < 32; i++) {
-            int x = a >> i;
-            int y = b >> i;
-            result += (x & 1) ^ (y & 1);
+            int cnt = 0;
+            for (int n : nums) {
+                cnt += (n >> i) & 1;
+            }
+            result += (nums.length - cnt) * cnt;
         }
         return result;
     }

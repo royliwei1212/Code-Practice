d38a3ef6566d905bab80be76f247f619c23aab65
Hao Zhou <i@hzhou.me>
2017-09-05 22:06:09 -0400

    add Second Minimum Node In a Binary Tree

---
+++ src/main/java/tech/saltyegg/leetcode/SecondMinimumNodeInABinaryTree.java
@@ -0,0 +1,33 @@
+package tech.saltyegg.leetcode;
+
+import tech.saltyegg.leetcode.parent.TreeNode;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author hzhou
+ * @since 9/5/17
+ */
+public class SecondMinimumNodeInABinaryTree {
+
+    public int findSecondMinimumValue(TreeNode root) {
+        if (root == null || root.left == null && root.right == null) return -1;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            heap.add(node.val);
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+        while (!heap.isEmpty() && heap.peek() == root.val) {
+            heap.poll();
+        }
+        return heap.isEmpty() ? -1 : heap.peek();
+    }
+
+}

4e902562282e9ec0f06c4a08bd545cce1a7f309b
Hao Zhou <i@hzhou.me>
2017-09-15 01:13:28 -0400

    add Find Leaves of Binary Tree

---
+++ src/main/java/tech/saltyegg/leetcode/FindLeavesOfBinaryTree.java
@@ -0,0 +1,32 @@
+package tech.saltyegg.leetcode;
+
+import tech.saltyegg.leetcode.parent.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author hzhou
+ * @since 9/15/17
+ */
+public class FindLeavesOfBinaryTree {
+
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        Map<Integer, List<Integer>> map = new TreeMap<>();
+        helper(root, map);
+        return new ArrayList<>(map.values());
+    }
+
+    private int helper(TreeNode root, Map<Integer, List<Integer>> map) {
+        if (root == null) return 0;
+        int depth = Math.max(helper(root.left, map), helper(root.right, map)) + 1;
+        if (!map.containsKey(depth)) {
+            map.put(depth, new ArrayList<>());
+        }
+        map.get(depth).add(root.val);
+        return depth;
+    }
+
+}

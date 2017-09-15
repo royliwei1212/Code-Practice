23f446fefd9af788540e7ff27a657e619e648a39
Hao Zhou <i@hzhou.me>
2017-09-15 19:55:37 -0400

    add Find Bottom Left Tree Value

---
+++ src/main/java/tech/saltyegg/leetcode/FindBottomLeftTreeValue.java
@@ -0,0 +1,44 @@
+package tech.saltyegg.leetcode;
+
+import tech.saltyegg.leetcode.parent.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author hzhou
+ * @since 9/15/17
+ */
+public class FindBottomLeftTreeValue {
+
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        int result = root.val;
+        queue.add(root);
+        queue.add(null);
+
+        boolean isPreNull = true;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                isPreNull = true;
+                if (!queue.isEmpty()) queue.add(null);
+                continue;
+            }
+            if (isPreNull) {
+                result = node.val;
+                isPreNull = false;
+            }
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+
+        }
+
+        return result;
+    }
+
+}

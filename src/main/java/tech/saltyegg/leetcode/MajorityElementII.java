3cb6f13e7418d6c347dd9dd0891eb13f99d5290a
Hao Zhou <i@hzhou.me>
2017-09-17 13:14:07 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/MajorityElementII.java
+++ src/main/java/tech/saltyegg/leetcode/MajorityElementII.java
@@ -1,16 +1,7 @@
-/**
- * Copyright (c) 2015 hzhou, All rights reserved.
- */
 package tech.saltyegg.leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * Description: Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. The algorithm
@@ -21,44 +12,25 @@ import static java.util.stream.Collectors.toList;
 public class MajorityElementII {
 
 	public List<Integer> majorityElement(int[] nums) {
-		List<Integer> result = new ArrayList<>();
-		if (nums == null || nums.length == 0) {
-			return result;
-		}
-		if (nums.length < 3) {
-			for (int i : nums) {
-				if (!result.contains(i)) {
-					result.add(i);
-				}
-			}
-			return result;
+        int a, b, ca, cb;
+        a = b = ca = cb = 0;
+        for (int n : nums) {
+            if(n == a) ca++;
+			else if (n == b) cb++;
+			else if(ca == 0) {a = n;ca++;}
+			else if(cb == 0) {b = n;cb++;}
+			else {ca--;cb--;}
 		}
 
-		int pivot = (int) Math.floor(nums.length / 3.0) + 1;
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i : nums) {
-			int value = 1;
-			if (map.containsKey(i)) {
-				value += map.get(i);
-				if (value == pivot) {
-					result.add(i);
-				}
-			}
-			map.put(i, value);
-		}
-
-		return result;
-	}
-
-	public List<Integer> majorityElement2(int[] nums) {
-		List<Integer> result = new ArrayList<Integer>();
-		if (nums == null || nums.length == 0) {
-			return result;
-		}
-		List<Integer> list = Arrays.stream(nums).boxed().collect(toList());
-		Map<Integer, Long> map = list.stream().collect(Collectors.groupingBy(o -> o, Collectors.counting()));
-		return map.entrySet().parallelStream().filter(v -> v.getValue() >= (int) Math.floor(list.size() / 3.0) + 1)
-				.map(Map.Entry::getKey).collect(toList());
-	}
+        ca = cb = 0;
+        for (int n : nums) {
+            if (n == a) ca++;
+            else if (n == b) cb++;
+        }
+        List<Integer> result = new ArrayList<>();
+        if (ca > nums.length / 3) result.add(a);
+        if (cb > nums.length / 3) result.add(b);
+        return result;
+    }
 
 }

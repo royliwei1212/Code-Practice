1aab94ad25fb7cacae159b8deeb3e8c8ffee5208
Hao Zhou <i@hzhou.me>
2017-09-23 12:04:16 -0400

    code cleanup

--- src/main/java/tech/saltyegg/leetcode/TextJustification.java
+++ src/main/java/tech/saltyegg/leetcode/TextJustification.java
@@ -1,106 +1,67 @@
-/**
- * Copyright (c) 2015 hzhou, All rights reserved.
- */
 package tech.saltyegg.leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertSame;
-
-/**
- * Description:
- *
- * @author hzhou
- */
 public class TextJustification {
 
-	public List<String> fullJustify(String[] words, int maxWidth) {
-		List<String> result = new ArrayList<String>();
-		if (words == null || words.length == 0) {
-			return result;
-		}
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        if (maxWidth == 0) return Collections.singletonList("");
+        int start = 0;
+        int length = 0;
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            String s = words[i];
+            if (length + s.length() <= maxWidth) {
+                length += s.length() + 1;
+            } else {
+                result.add(helper(words, start, i - 1, length - 1, maxWidth));
+                length = s.length() + 1;
+                start = i;
+            }
+        }
+        result.add(lastLine(words, start, maxWidth));
+        return result;
+    }
 
-		if (maxWidth == 0) {
-			result.add("");
-			return result;
-		}
+    private String lastLine(String[] words, int start, int maxWidth) {
+        StringBuilder sb = new StringBuilder();
+        while (start < words.length) {
+            sb.append(words[start]);
+            if (sb.length() < maxWidth) sb.append(" ");
+            start++;
+        }
+        appendSpace(sb, maxWidth - sb.length());
+        return sb.toString();
+    }
 
-		int crt = 0;
-		int pre = 0;
-		int count = 0;
+    private String helper(String[] words, int start, int end, int length, int maxWidth) {
+        StringBuilder sb = new StringBuilder();
+        if (end == start) {
+            sb.append(words[start]);
+            appendSpace(sb, maxWidth - words[start].length());
+            return sb.toString();
+        }
 
-		while (pre < words.length) {
+        int totalPadding = maxWidth - (length - end + start);
+        int interval = end - start;
+        int step = totalPadding / interval + (totalPadding % interval > 0 ? 1 : 0);
+        for (int i = start; i <= end; i++) {
+            sb.append(words[i]);
+            appendSpace(sb, Math.min(step, totalPadding));
+            totalPadding -= step;
+            interval--;
+            if (interval > 0) step = totalPadding / interval + (totalPadding % interval > 0 ? 1 : 0);
+        }
+        return sb.toString();
+    }
 
-			count = words[crt].length() + 1;
-			while (count <= maxWidth + 1 && crt < words.length - 1) {
-				crt++;
-				count += words[crt].length() + 1;
-			}
-
-			// for the last line
-			if (crt == words.length - 1 && count <= maxWidth + 1) {
-				StringBuilder sb = new StringBuilder();
-				for (int i = pre; i < words.length - 1; i++) {
-					sb.append(words[i]).append(' ');
-				}
-				sb.append(words[words.length - 1]);
-				result.add(appendSpace(sb.toString(), maxWidth));
-				break;
-			}
-
-			if (crt - pre == 1) {
-				result.add(appendSpace(words[pre], maxWidth));
-			} else {
-				count = count - words[crt].length() - 2;
-				int realLength = maxWidth - count;
-				int spaceCount = realLength / (crt - pre - 1) + 1;
-				int extra = realLength % (crt - pre - 1);
-				result.add(getLine(words, pre, crt - pre, spaceCount, extra));
-			}
-
-			pre = crt;
-		}
-
-		return result;
-	}
-
-	private String getLine(String[] words, int start, int count, int spaceCount, int extra) {
-		StringBuilder sb = new StringBuilder();
-		StringBuilder _sb = new StringBuilder();
-		for (int i = 0; i < spaceCount; i++) {
-			_sb.append(' ');
-		}
-		String spaceStr = _sb.toString();
-
-		for (int i = start; i < start + count - 1; i++) {
-			sb.append(words[i]).append(spaceStr);
-			if (extra-- > 0) {
-				sb.append(' ');
-			}
-		}
-		sb.append(words[start + count - 1]);
-		return sb.toString();
-	}
-
-	private String appendSpace(String s, int maxWidth) {
-		StringBuilder sb = new StringBuilder(s);
-		while (sb.length() < maxWidth) {
-			sb.append(' ');
-		}
-		return sb.toString();
-	}
-
-	@Test
-	public void test() {
-		String[] words = new String[]{"This", "is", "an", "example", "of", "text", "justification."};
-		List<String> result = fullJustify(words, 16);
-		assertSame("example  of text", result.get(1));
-
-		words = new String[]{"a", "b", "c", "d", "e"};
-		result = fullJustify(words, 1);
-
-	}
+    private void appendSpace(StringBuilder sb, int count) {
+        if (count <= 0) return;
+        while (count > 0) {
+            sb.append(" ");
+            count--;
+        }
+    }
 }

37ad9bceb81a35e48c63309eb2dbac037c1451e7
Hao Zhou <i@hzhou.me>
2017-09-23 14:06:38 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/MaxPointsOnALine.java
+++ src/main/java/tech/saltyegg/leetcode/MaxPointsOnALine.java
@@ -1,59 +1,50 @@
 package tech.saltyegg.leetcode;
 
+import tech.saltyegg.leetcode.parent.Point;
+
 import java.util.HashMap;
 import java.util.Map;
-
-import tech.saltyegg.leetcode.parent.Point;
 
 /**
  * Created by hzhou on 2015/6/1. Email: codeashobby@gmail.com
  */
 public class MaxPointsOnALine {
 
-	public int maxPoints(Point[] points) {
-		if (points == null || points.length == 0) {
-			return 0;
-		}
+    public int maxPoints(Point[] points) {
+        if (points == null || points.length == 0) return 0;
+        if (points.length < 3) return points.length;
 
-		if (points.length < 3) {
-			return points.length;
-		}
+        int result = 2;
+        for (int i = 0; i < points.length - 1; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            int same = 0;
+            Point p = points[i];
+            for (int j = i + 1; j < points.length; j++) {
+                Point ps = points[j];
+                if (p.x == ps.x && p.y == ps.y) {
+                    same++;
+                } else if (p.x == ps.x) {
+                    map.put("Double.POSITIVE_INFINITY", map.getOrDefault("Double.POSITIVE_INFINITY", 1) + 1);
+                } else if (p.y == ps.y) {
+                    map.put("0.0", map.getOrDefault("0.0", 1) + 1);
+                } else {
+                    int y = p.y - ps.y;
+                    int x = p.x - ps.x;
+                    int gcd = gcd(y, x);
+                    String key = y / gcd + "-" + x / gcd;
+                    map.put(key, map.getOrDefault(key, 1) + 1);
+                }
+            }
+            int localMax = 1;
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                localMax = Math.max(localMax, entry.getValue());
+            }
+            result = Math.max(result, same + localMax);
+        }
+        return result;
+    }
 
-		int globalMax = 0;
-
-		Map<Double, Integer> map = new HashMap<Double, Integer>();
-		for (int i = 0; i < points.length; i++) {
-			map.clear();
-			int localMax = 0;
-			int samePoints = 0;
-			Point p1 = points[i];
-			for (int j = i + 1; j < points.length; j++) {
-				Point p2 = points[j];
-				double slope = 0;
-				if (p1.x == p2.x && p1.y == p2.y) {
-					samePoints++;
-					continue; // this is important
-				} else if (p1.y == p2.y) {
-					slope = 0;
-				} else if (p1.x == p2.x) {
-					slope = Double.MAX_VALUE;
-				} else {
-					slope = (p1.y * 1.0 - p2.y) / (p1.x * 1.0 - p2.x);
-				}
-				if (map.containsKey(slope)) {
-					map.put(slope, map.get(slope) + 1);
-					localMax = Math.max(localMax, map.get(slope));
-				} else {
-					map.put(slope, 1);
-					if (localMax == 0) {
-						localMax = 1;
-					}
-				}
-			}
-
-			globalMax = Math.max(globalMax, localMax + samePoints + 1);
-		}
-
-		return globalMax;
-	}
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
 }

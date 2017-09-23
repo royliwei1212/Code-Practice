419d397cbd5dcbbae005f44e767bebd57ef0e104
Hao Zhou <i@hzhou.me>
2017-09-22 23:01:11 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/PowX.java
+++ src/main/java/tech/saltyegg/leetcode/PowX.java
@@ -5,18 +5,17 @@ package tech.saltyegg.leetcode;
  */
 public class PowX {
 
-	public double myPow(double x, int n) {
-		boolean isPositive = n > 0;
-		double result = 1.0;
-		n = n > 0 ? n : -n;
-		while (n > 0) {
-			if ((n & 1) == 1) {
-				result *= x;
-			}
-			x *= x;
-			n = n >> 1;
-		}
-
-		return isPositive ? result : (1.0 / result);
-	}
+    public double myPow(double x, int n) {
+        boolean isNeg = n < 0;
+        long m = Math.abs((long) n);
+        double result = 1;
+        while (m > 0) {
+            if ((m & 1) == 1) {
+                result *= x;
+            }
+            x *= x;
+            m = m >> 1;
+        }
+        return isNeg ? (1 / result) : result;
+    }
 }

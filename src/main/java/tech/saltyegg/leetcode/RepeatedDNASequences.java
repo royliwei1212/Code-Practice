c71ef99039105e93c875e73a8653a3a6a9c9e3fc
Hao Zhou <i@hzhou.me>
2017-09-24 18:18:32 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/RepeatedDNASequences.java
+++ src/main/java/tech/saltyegg/leetcode/RepeatedDNASequences.java
@@ -8,8 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Test;
-
 /**
  * Description
  *
@@ -17,74 +15,19 @@ import org.junit.Test;
  */
 public class RepeatedDNASequences {
 
-	//TODO: wrong answer
-	public List<String> findRepeatedDnaSequences(String s) {
-		List<String> result = new ArrayList<String>();
-		Map<Long, Integer> map = new HashMap<Long, Integer>();
-		if (s == null || s.isEmpty() || s.length() < 10) {
-			return result;
-		}
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        if (s == null || s.isEmpty()) return result;
+        Map<String, Integer> dp = new HashMap<>();
+        for (int i = 0; i < s.length() - 9; i++) {
+            String sub = s.substring(i, i + 10);
+            dp.put(sub, dp.getOrDefault(sub, 0) + 1);
+        }
 
-		for (int i = 0; i < s.length() - 9; i++) {
-			String tmp = s.substring(i, i + 10);
-			Long key = str2long(tmp);
-			if (map.containsKey(key)) {
-				map.put(key, map.get(key) + 1);
-			} else {
-				map.put(key, 1);
-			}
-		}
-
-		for (Long key : map.keySet()) {
-			if (map.get(key) > 1) {
-				result.add(long2str(key));
-			}
-		}
-		return result;
-	}
-
-	private long str2long(String s) {
-		long res = 0;
-		for (int i = 0; i < 10; i++) {
-			if (s.charAt(i) == 'A') {
-				res = res * 10 + 1;
-			}
-			if (s.charAt(i) == 'T') {
-				res = res * 10 + 2;
-			}
-			if (s.charAt(i) == 'C') {
-				res = res * 10 + 3;
-			}
-			if (s.charAt(i) == 'G') {
-				res = res * 10 + 4;
-			}
-		}
-		return res;
-	}
-
-	private String long2str(long s) {
-		StringBuilder res = new StringBuilder();
-		for (int i = 0; i < 10; i++) {
-			long d = s % 10;
-			if (d == 1) {
-				res.insert(0, 'A');
-			}
-			if (d == 2) {
-				res.insert(0, 'T');
-			}
-			if (d == 3) {
-				res.insert(0, 'C');
-			}
-			if (d == 4) {
-				res.insert(0, 'G');
-			}
-			s = s / 10;
-		}
-		return res.toString();
-	}
-
-	@Test
-	public void test() {
-		List<String> result = findRepeatedDnaSequences("AAAAAAAAAAA");
-	}
+        for (Map.Entry<String, Integer> entry : dp.entrySet()) {
+            if (entry.getValue() < 2) continue;
+            result.add(entry.getKey());
+        }
+        return result;
+    }
 }

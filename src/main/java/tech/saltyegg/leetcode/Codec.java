780b51f47b273dc48923b0b4d83cf041177ca531
Hao Zhou <i@hzhou.me>
2017-09-28 13:05:25 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/Codec.java
+++ src/main/java/tech/saltyegg/leetcode/Codec.java
@@ -2,7 +2,8 @@ package tech.saltyegg.leetcode;
 
 import tech.saltyegg.leetcode.parent.TreeNode;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @author hzhou
@@ -13,78 +14,54 @@ public class Codec {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         if (root == null) return "";
-        return root.val + "(" + serialize(root.left) + "," + serialize(root.right) + ")";
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        sb.append(root.val).append(",");
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left == null) {
+                sb.append("#");
+            } else {
+                sb.append(node.left.val);
+                queue.add(node.left);
+            }
+            sb.append(",");
+            if (node.right == null) {
+                sb.append("#");
+            } else {
+                sb.append(node.right.val);
+                queue.add(node.right);
+            }
+            sb.append(",");
+        }
+        return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if (data == null || data.isEmpty() || !(Character.isDigit(data.charAt(0)) || data.charAt(0) == '-')) return null;
-        int x = parseInt(data);
-        TreeNode root = new TreeNode(x);
-        _deserialize(data.substring(data.indexOf('(') + 1, data.length() - 1), root);
+        if (data == null || data.isEmpty()) return null;
+        String[] splits = data.trim().split(",");
+        if (splits == null || splits.length == 0) return null;
+
+        int start = 0;
+        TreeNode root = new TreeNode(Integer.parseInt(splits[start++]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            String a = splits[start++];
+            String b = splits[start++];
+            if (!a.equals("#")) {
+                node.left = new TreeNode(Integer.parseInt(a));
+                queue.add(node.left);
+            }
+            if (!b.equals("#")) {
+                node.right = new TreeNode(Integer.parseInt(b));
+                queue.add(node.right);
+            }
+        }
         return root;
-    }
-
-    private void _deserialize(String data, TreeNode root) {
-        if (data == null || data.isEmpty()) return;
-
-        if (data.equals(",")) {
-            root.left = root.right = null;
-            return;
-        }
-
-        Stack<Character> stack = new Stack<>();
-        int splitPoint = -1;
-        for (int i = 0; i < data.length(); i++) {
-            char c = data.charAt(i);
-            if (c == ',' && stack.isEmpty()) {
-                splitPoint = i;
-                break;
-            }
-            if (c == '(') stack.push('(');
-            if (c == ')') stack.pop();
-
-        }
-        if (splitPoint == -1) return;
-
-        String left = (splitPoint == 0 ? null : data.substring(0, splitPoint));
-        String right = (splitPoint == data.length() - 1 ? null : data.substring(splitPoint + 1, data.length()));
-
-        Integer integer = parseInt(left);
-        if (integer == null) {
-            root.left = null;
-        } else {
-            TreeNode l = new TreeNode(integer);
-            root.left = l;
-            if (left.contains("(")) _deserialize(left.substring(left.indexOf('(') + 1, left.length() - 1), l);
-        }
-
-        integer = parseInt(right);
-        if (integer == null) {
-            root.right = null;
-        } else {
-            TreeNode r = new TreeNode(integer);
-            root.right = r;
-            if (right.contains("(")) _deserialize(right.substring(right.indexOf('(') + 1, right.length() - 1), r);
-        }
-    }
-
-    private Integer parseInt(String serial) {
-        if (serial == null || serial.isEmpty() ||
-                !(Character.isDigit(serial.charAt(0)) || serial.charAt(0) == '-')) {
-            return null;
-        }
-        int result = 0;
-        boolean isNegative = (serial.charAt(0) == '-');
-        for (int i = isNegative ? 1 : 0; i < serial.length(); i++) {
-            char c = serial.charAt(i);
-            if (Character.isDigit(c)) {
-                result = 10 * result + (c - '0');
-            } else {
-                break;
-            }
-        }
-        return result * (isNegative ? -1 : 1);
     }
 
 

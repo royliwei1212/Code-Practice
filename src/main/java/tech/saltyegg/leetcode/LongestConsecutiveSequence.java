1d52e426c6ff9180c14557e011cd4a60e95faf8d
Hao Zhou <i@hzhou.me>
2017-09-30 20:26:38 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/LongestConsecutiveSequence.java
+++ src/main/java/tech/saltyegg/leetcode/LongestConsecutiveSequence.java
@@ -3,10 +3,10 @@
  */
 package tech.saltyegg.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertSame;
 
@@ -22,44 +22,26 @@ import static org.junit.Assert.assertSame;
  */
 public class LongestConsecutiveSequence {
 
-	public int longestConsecutive(int[] nums) {
-		if (nums == null || nums.length == 0) {
-			return 0;
-		}
-		if (nums.length == 1) {
-			return 1;
-		}
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        Map<Integer, Integer> dp = new HashMap<>();
+        int result = 1;
+        for (int n : nums) {
+            if (dp.containsKey(n)) continue;
+            int left = dp.getOrDefault(n - 1, 0);
+            int right = dp.getOrDefault(n + 1, 0);
+            int sum = left + right + 1;
+            result = Math.max(result, sum);
+            dp.put(n, sum);
+            dp.put(n - left, sum);
+            dp.put(n + right, sum);
+        }
+        return result;
+    }
 
-		Set<Integer> set = new HashSet<Integer>();
-
-		for (int i : nums) {
-			set.add(i);
-		}
-
-		int result = 0;
-
-		for (int i : nums) {
-			int l = i - 1;
-			int r = i + 1;
-			int count = 1;
-			while (set.contains(l)) {
-				count++;
-				set.remove(l);
-				l--;
-			}
-			while (set.contains(r)) {
-				count++;
-				//set.remove(r);
-				r++;
-			}
-			result = Math.max(result, count);
-		}
-		return result;
-	}
-
-	@Test
-	public void test() {
-		int[] nums = new int[]{2147483646, -2147483647, 0, 2, 2147483644, -2147483645, 2147483645};
-		assertSame(3, longestConsecutive(nums));
-	}
+    @Test
+    public void test() {
+        int[] nums = new int[]{2147483646, -2147483647, 0, 2, 2147483644, -2147483645, 2147483645};
+        assertSame(3, longestConsecutive(nums));
+    }
 }

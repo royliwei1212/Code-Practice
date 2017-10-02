aef74b532703b8071d851afed64d34ab1e295bd9
Hao Zhou <i@hzhou.me>
2017-10-01 21:35:34 -0400

    code update

--- src/main/java/tech/saltyegg/leetcode/LargestRectangleInHistogram.java
+++ src/main/java/tech/saltyegg/leetcode/LargestRectangleInHistogram.java
@@ -3,44 +3,26 @@
  */
 package tech.saltyegg.leetcode;
 
-import java.util.Arrays;
 import java.util.Stack;
 
-import org.junit.Test;
-
 /**
- * Description: TODO: This is a hard question
- *
- * http://www.cnblogs.com/lichen782/p/leetcode_Largest_Rectangle_in_Histogram.html
- *
- * @author hzhou
+ * http://www.cnblogs.com/grandyang/p/4322653.html
  */
 public class LargestRectangleInHistogram {
 
-	public int largestRectangleArea(int[] height) {
-		if (height == null || height.length == 0) {
-			return 0;
-		}
-		Stack<Integer> stack = new Stack<Integer>();
-		int[] arr = Arrays.copyOf(height, height.length + 1);
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) return 0;
+        int result = 0;
 
-		int crt = 0;
-		int max = 0;
-		while (crt < arr.length) {
-			if (stack.isEmpty() || arr[crt] >= arr[stack.peek()]) {
-				stack.push(crt++);
-			} else {
-				int i = stack.pop();
-				max = Math.max(max, arr[i] * (stack.isEmpty() ? crt : crt - stack.peek() - 1));
-			}
-		}
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <= heights.length; i++) {
+            while (!stack.isEmpty() && (i == heights.length || heights[i] <= heights[stack.peek()])) {
+                int h = heights[stack.pop()];
+                result = Math.max(result, h * (stack.isEmpty() ? i : i - 1 - stack.peek()));
+            }
+            stack.push(i);
+        }
 
-		return max;
-	}
-
-	@Test
-	public void test() {
-		int[] nums = new int[]{3, 3, 2, 2};
-		int result = largestRectangleArea(nums);
-	}
+        return result;
+    }
 }

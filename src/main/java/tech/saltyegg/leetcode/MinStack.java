7f1683d054e509d81bf955e408a14d519ff7987a
Hao Zhou <i@hzhou.me>
2017-10-03 19:02:35 -0400

    code update

--- src/main/java/tech/saltyegg/leetcode/MinStack.java
+++ src/main/java/tech/saltyegg/leetcode/MinStack.java
@@ -7,28 +7,28 @@ import java.util.Stack;
  */
 public class MinStack {
 
-	private Stack<Integer> stack = new Stack<Integer>();
-	private Stack<Integer> minStack = new Stack<Integer>();
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
 
-	public void push(int x) {
-		stack.push(x);
-		if (minStack.isEmpty() || x <= minStack.peek()) {
-			minStack.push(x);
-		}
-	}
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
 
-	public void pop() {
-		int val = stack.pop();
-		if (val <= minStack.peek()) {
-			minStack.pop();
-		}
-	}
+    public void pop() {
+        int val = stack.pop();
+        if (val <= minStack.peek()) {
+            minStack.pop();
+        }
+    }
 
-	public int top() {
-		return stack.peek();
-	}
+    public int top() {
+        return stack.peek();
+    }
 
-	public int getMin() {
-		return minStack.peek();
-	}
+    public int getMin() {
+        return minStack.peek();
+    }
 }

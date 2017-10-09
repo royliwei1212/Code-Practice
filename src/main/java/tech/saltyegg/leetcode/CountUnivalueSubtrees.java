bdfbb4abe2155e6686ac28fd6375dea2c12592a1
Hao Zhou <i@hzhou.me>
2017-10-09 12:21:53 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/CountUnivalueSubtrees.java
+++ src/main/java/tech/saltyegg/leetcode/CountUnivalueSubtrees.java
@@ -1,7 +1,6 @@
 package tech.saltyegg.leetcode;
 
 import tech.saltyegg.leetcode.parent.TreeNode;
-import org.junit.Test;
 
 /**
  * Created by hzhou on 2015/8/8.
@@ -10,60 +9,17 @@ import org.junit.Test;
 public class CountUnivalueSubtrees {
 
     public int countUnivalSubtrees(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        if (root.left == null && root.right == null) {
-            return 1;
-        }
-
+        if (root == null) return 0;
         int[] result = new int[]{0};
-        helper(root, result);
+        isUnival(root, root.val, result);
         return result[0];
     }
 
-    private boolean helper(TreeNode root, int[] result) {
-
-        if (root.right == null && root.left == null) {
-            result[0]++;
-            return true;
-        } else if (root.right != null && root.left == null) {
-            if (helper(root.right, result) && root.val == root.right.val) {
-                result[0]++;
-                return true;
-            } else {
-                return false;
-            }
-        } else if (root.right == null) {
-            if (helper(root.left, result) && root.val == root.left.val) {
-                result[0]++;
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            boolean l = helper(root.right, result);
-            boolean r = helper(root.left, result);
-            if (l && r && root.val == root.left.val && root.val == root.right.val) {
-                result[0]++;
-                return true;
-            } else {
-                return false;
-            }
-        }
-
+    private boolean isUnival(TreeNode root, int val, int[] result) {
+        if (root == null) return true;
+        if (!isUnival(root.left, root.val, result) | !isUnival(root.right, root.val, result)) return false;
+        result[0]++;
+        return root.val == val;
     }
 
-    @Test
-    public void test() {
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(1);
-        root.left.left = new TreeNode(5);
-        root.left.right = new TreeNode(5);
-        root.right = new TreeNode(5);
-        root.right.right = new TreeNode(5);
-
-        countUnivalSubtrees(root);
-    }
 }

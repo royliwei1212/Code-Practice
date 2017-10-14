b46a9782be6fa6057fe6539353f1335090902552
Hao Zhou <i@hzhou.me>
2017-10-14 18:55:35 -0400

    remove test code

--- src/main/java/tech/saltyegg/leetcode/Codec.java
+++ src/main/java/tech/saltyegg/leetcode/Codec.java
@@ -63,21 +63,4 @@ public class Codec {
         }
         return root;
     }
-
-
-    public static void main(String[] args) {
-        Codec c = new Codec();
-        TreeNode n1 = new TreeNode(-1);
-        TreeNode n2 = new TreeNode(0);
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n4 = new TreeNode(4);
-        TreeNode n5 = new TreeNode(5);
-        n1.left = n2;
-        n1.right = n3;
-//        n3.left = n4;
-//        n3.right = n5;
-        String ser = c.serialize(n1);
-        System.out.println(ser);
-        TreeNode root = c.deserialize(ser);
-    }
 }

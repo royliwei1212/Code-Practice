a43057717bebb3186cd10cab4d1153f0ee3d7d1e
Hao Zhou <i@hzhou.me>
2017-10-14 13:52:42 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/tech/saltyegg/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,46 +1,25 @@
 package tech.saltyegg.leetcode;
 
-import org.junit.Test;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.Assert.assertSame;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.isEmpty()) {
-            return 0;
-        }
-
-        int pre, result;
-        pre = 0;
-        result = 1;
-
-        Set<Character> set = new HashSet<>();
-        set.add(s.charAt(0));
+        if (s == null || s.isEmpty()) return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int result = 1;
+        int pre = -1;
+        map.put(s.charAt(0), 0);
         for (int i = 1; i < s.length(); i++) {
-
             char c = s.charAt(i);
-            if (set.contains(c)) {
-
-                while (s.charAt(pre) != s.charAt(i)) {
-                    set.remove(s.charAt(pre));
-                    pre++;
-                }
-                pre++;
-            } else {
-                set.add(c);
+            int lastIndex = map.getOrDefault(c, -1);
+            if (lastIndex > pre) {
+                pre = lastIndex;
             }
-            result = Math.max(result, i - pre + 1);
+            map.put(c, i);
+            result = Math.max(result, i - pre);
         }
         return result;
-    }
-
-    @Test
-    public void test() {
-        String s = "abb";
-        assertSame(2, lengthOfLongestSubstring(s));
     }
 }

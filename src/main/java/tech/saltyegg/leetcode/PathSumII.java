1a25db6ddde12f9c00b8f5221d150dbe53db5c19
hzhou <i@hzhou.me>
2017-10-20 11:03:09 -0400

    code update

--- src/main/java/tech/saltyegg/leetcode/PathSumII.java
+++ src/main/java/tech/saltyegg/leetcode/PathSumII.java
@@ -8,31 +8,28 @@ import java.util.List;
 /**
  * Created by hzhou on 4/22/15. codeashobby@gmail.com
  */
+@SuppressWarnings("Duplicates")
 public class PathSumII {
 
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        if (root == null) {
-            return result;
-        }
-        List<Integer> crt = new ArrayList<Integer>();
-        helper(root, crt, result, sum);
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        helper(root, new ArrayList<>(), result, sum);
         return result;
     }
 
     private void helper(TreeNode root, List<Integer> crt, List<List<Integer>> result, int target) {
-        if (root == null) {
-            return;
-        }
+        if (root == null) return;
+
         crt.add(root.val);
         if (root.left == null && root.right == null) {
             if (root.val == target) {
-                List<Integer> tmp = new ArrayList<Integer>(crt);
-                result.add(tmp);
+                result.add(new ArrayList<>(crt));
             }
         } else {
             if (root.left != null) {
-                helper(root.left, new ArrayList<Integer>(crt), result, target - root.val);
+                helper(root.left, new ArrayList<>(crt), result, target - root.val);
             }
 
             if (root.right != null) {

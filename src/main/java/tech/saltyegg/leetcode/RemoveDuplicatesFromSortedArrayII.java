8ff7b1c42075eb1f10b89a2a25da4d7ffba0ee83
Hao Zhou <i@hzhou.me>
2017-10-29 18:27:16 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/RemoveDuplicatesFromSortedArrayII.java
+++ src/main/java/tech/saltyegg/leetcode/RemoveDuplicatesFromSortedArrayII.java
@@ -1,7 +1,5 @@
 package tech.saltyegg.leetcode;
 
-import org.junit.Test;
-
 /**
  * Created by hzhou on 2015/5/27.
  * Email: codeashobby@gmail.com
@@ -14,37 +12,23 @@ import org.junit.Test;
 public class RemoveDuplicatesFromSortedArrayII {
 
     public int removeDuplicates(int[] nums) {
-        if (nums == null) {
-            return 0;
-        }
-        if (nums.length < 3) {
-            return nums.length;
-        }
-
-        int crtVal = nums[0];
-        int crtCount = 1;
-        int length = 1;
+        if (nums == null || nums.length < 3) return nums == null ? 0 : nums.length;
+        int pre = nums[0];
+        int count = 1;
+        int l = 1;
         for (int i = 1; i < nums.length; i++) {
-            int crt = nums[i];
-            nums[length++] = crt;
-            if (crt == crtVal) {
-                crtCount++;
-                while (crtCount >= 2 && i < nums.length && nums[i] == crtVal) {
-                    i++;
-                }
-                i--;
+            int c = nums[i];
+            if (pre != c) {
+                nums[l++] = c;
+                pre = c;
+                count = 1;
+            } else if (count < 2) {
+                count++;
+                nums[l++] = pre;
             } else {
-                crtVal = crt;
-                crtCount = 1;
+                count++;
             }
         }
-
-        return length;
-    }
-
-    @Test
-    public void test() {
-        int[] nums = new int[]{1, 1, 1};
-        int result = removeDuplicates(nums);
+        return l;
     }
 }

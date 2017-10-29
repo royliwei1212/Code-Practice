4c75ef448c28a47a7eec57da51a4aa7eb3da4d9f
Hao Zhou <i@hzhou.me>
2017-10-29 16:05:19 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/IntegerToEnglishWords.java
+++ src/main/java/tech/saltyegg/leetcode/IntegerToEnglishWords.java
@@ -1,8 +1,6 @@
 package tech.saltyegg.leetcode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,97 +8,75 @@ import java.util.Map;
  * Email: i@hzhou.me
  */
 public class IntegerToEnglishWords {
-    private static final Map<Integer, String> intMap = new HashMap<>();
-    private static final List<String> list = new ArrayList<>();
+    private final Map<Integer, String> map;
+    private final String[] list;
 
-    static {
-        intMap.put(0, "Zero");
-        intMap.put(1, "One");
-        intMap.put(2, "Two");
-        intMap.put(3, "Three");
-        intMap.put(4, "Four");
-        intMap.put(5, "Five");
-        intMap.put(6, "Six");
-        intMap.put(7, "Seven");
-        intMap.put(8, "Eight");
-        intMap.put(9, "Nine");
-        intMap.put(10, "Ten");
-        intMap.put(11, "Eleven");
-        intMap.put(12, "Twelve");
-        intMap.put(13, "Thirteen");
-        intMap.put(14, "Fourteen");
-        intMap.put(15, "Fifteen");
-        intMap.put(16, "Sixteen");
-        intMap.put(17, "Seventeen");
-        intMap.put(18, "Eighteen");
-        intMap.put(19, "Nineteen");
-        intMap.put(20, "Twenty");
-        intMap.put(30, "Thirty");
-        intMap.put(40, "Forty");
-        intMap.put(50, "Fifty");
-        intMap.put(60, "Sixty");
-        intMap.put(70, "Seventy");
-        intMap.put(80, "Eighty");
-        intMap.put(90, "Ninety");
-
-        list.add("");
-        list.add("Thousand");
-        list.add("Million");
-        list.add("Billion");
+    public IntegerToEnglishWords() {
+        map = new HashMap<>();
+        map.put(0, "Zero");
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        map.put(4, "Four");
+        map.put(5, "Five");
+        map.put(6, "Six");
+        map.put(7, "Seven");
+        map.put(8, "Eight");
+        map.put(9, "Nine");
+        map.put(10, "Ten");
+        map.put(11, "Eleven");
+        map.put(12, "Twelve");
+        map.put(13, "Thirteen");
+        map.put(14, "Fourteen");
+        map.put(15, "Fifteen");
+        map.put(16, "Sixteen");
+        map.put(17, "Seventeen");
+        map.put(18, "Eighteen");
+        map.put(19, "Nineteen");
+        map.put(20, "Twenty");
+        map.put(30, "Thirty");
+        map.put(40, "Forty");
+        map.put(50, "Fifty");
+        map.put(60, "Sixty");
+        map.put(70, "Seventy");
+        map.put(80, "Eighty");
+        map.put(90, "Ninety");
+        list = new String[]{" Billion ", " Million ", " Thousand ", ""};
     }
 
     public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        int i = 3;
         StringBuilder sb = new StringBuilder();
-        int index = 0;
         while (num > 0) {
             int n = num % 1000;
-            sb.insert(0, toStr(n, index));
-            num = num / 1000;
-            index++;
+            String s = hundredToString(n);
+            if (!s.isEmpty()) {
+                s += list[i];
+                sb.insert(0, s);
+            }
+            i--;
+            num /= 1000;
         }
-
-        return sb.length() == 0 ? "Zero" : sb.toString().replace("  ", " ").trim();
+        return sb.toString().trim();
     }
 
-    // num < 1000
-    private String toStr(int num, int index) {
-        if (num == 0) {
-            return "";
-        }
-
+    private String hundredToString(int n) {
+        if (n == 0) return "";
         StringBuilder sb = new StringBuilder();
-        int n = num / 100;
-        if (n > 0) {
-            sb.append(intMap.get(n)).append(" Hundred ");
+        int x = n / 100;
+        if (x > 0) {
+            sb.append(map.get(x)).append(" Hundred");
         }
-
-        num = num % 100;
-        n = num / 10;
-        if (n == 1) {
-            sb.append(intMap.get(num));
+        int y = (n - x * 100);
+        if (y == 0) return sb.toString();
+        if (y <= 20 || y % 10 == 0) {
+            if (sb.length() > 0) sb.append(" ");
+            sb.append(map.get(y));
         } else {
-            if (n != 0) {
-                sb.append(intMap.get(n * 10));
-            }
-            n = num % 10;
-            if (n != 0) {
-                sb.append(" ").append(intMap.get(n));
-            }
+            if (sb.length() > 0) sb.append(" ");
+            sb.append(map.get(y - n % 10)).append(" ").append(map.get(n % 10));
         }
-        sb.append(" ").append(list.get(index)).append(" ");
         return sb.toString();
-    }
-
-
-    public static void main(String[] args) {
-        IntegerToEnglishWords itew = new IntegerToEnglishWords();
-        System.out.println(itew.toStr(0, 0));
-        System.out.println(itew.toStr(100, 0));
-        System.out.println(itew.toStr(109, 0));
-        System.out.println(itew.toStr(9, 1));
-        System.out.println(itew.toStr(999, 0));
-
-        System.out.println(itew.numberToWords(50868));
-        System.out.println(itew.numberToWords(1234567));
     }
 }

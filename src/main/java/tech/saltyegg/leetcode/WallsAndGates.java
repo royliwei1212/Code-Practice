cc3c834eea21ebb136a2016761c4a6b3eb622d62
Hao Zhou <i@hzhou.me>
2017-10-30 00:38:30 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/WallsAndGates.java
+++ src/main/java/tech/saltyegg/leetcode/WallsAndGates.java
@@ -7,29 +7,24 @@ package tech.saltyegg.leetcode;
 public class WallsAndGates {
 
     public void wallsAndGates(int[][] rooms) {
+        if (rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0) return;
         for (int i = 0; i < rooms.length; i++) {
             for (int j = 0; j < rooms[0].length; j++) {
                 if (rooms[i][j] == 0) {
-                    dfs(i + 1, j, 1, rooms);
-                    dfs(i - 1, j, 1, rooms);
-                    dfs(i, j + 1, 1, rooms);
-                    dfs(i, j - 1, 1, rooms);
+                    helper(rooms, i, j, 0);
                 }
             }
         }
-
     }
 
-
-    private void dfs(int x, int y, int val, int[][] rooms) {
-        if (x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length) return;
-
-        if (rooms[x][y] > val) {
-            rooms[x][y] = val;
-            dfs(x + 1, y, val + 1, rooms);
-            dfs(x - 1, y, val + 1, rooms);
-            dfs(x, y + 1, val + 1, rooms);
-            dfs(x, y - 1, val + 1, rooms);
+    private void helper(int[][] rooms, int x, int y, int d) {
+        if (x < 0 || y < 0 || x >= rooms.length || y >= rooms[0].length || rooms[x][y] == -1) return;
+        if (d == 0 || rooms[x][y] > d) {
+            rooms[x][y] = d;
+            helper(rooms, x + 1, y, d + 1);
+            helper(rooms, x - 1, y, d + 1);
+            helper(rooms, x, y + 1, d + 1);
+            helper(rooms, x, y - 1, d + 1);
         }
     }
 

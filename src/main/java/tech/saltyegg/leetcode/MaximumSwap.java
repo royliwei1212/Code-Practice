357dd654c5b877840cb99b3ea89d5c08d2b751a2
Hao Zhou <i@hzhou.me>
2017-10-29 22:16:17 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/MaximumSwap.java
+++ src/main/java/tech/saltyegg/leetcode/MaximumSwap.java
@@ -1,8 +1,5 @@
 package tech.saltyegg.leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 /**
  * @author hzhou
  * @since 9/5/17
@@ -10,47 +7,29 @@ import java.util.Comparator;
 public class MaximumSwap {
 
     public int maximumSwap(int num) {
-        Integer[] arr = new Integer[("" + num).length()];
-        Integer[] nums = new Integer[arr.length];
-
-        int copy = num;
-        int i = 0;
-        while (copy > 0) {
-            int mod = copy % 10;
-            arr[i] = mod;
-            nums[arr.length - i - 1] = mod;
-            i++;
-            copy /= 10;
+        if (num < 10) return num;
+        String numStr = num + "";
+        int[] arr = new int[numStr.length()];
+        for (int i = 0; i < numStr.length(); i++) arr[i] = numStr.charAt(i) - '0';
+        int[] dp = new int[numStr.length()];
+        dp[dp.length - 1] = num % 10;
+        for (int i = dp.length - 2; i >= 0; i--) {
+            dp[i] = Math.max(dp[i + 1], arr[i]);
         }
-        Arrays.sort(arr, Comparator.reverseOrder());
-
-        int swapMax, toSwap;
-        swapMax = toSwap = 0;
-
-        for (i = 0; i < arr.length; i++) {
-            if (!arr[i].equals(nums[i])) {
-                swapMax = arr[i];
-                toSwap = nums[i];
+        for (int i = 0; i < dp.length; i++) {
+            if (dp[i] == arr[i]) continue;
+            for (int j = dp.length - 1; j >= 0; j--) {
+                if (dp[j] != dp[i]) continue;
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
                 break;
             }
-        }
-        if (i == arr.length) return num;
-
-        for (i = 0; i < nums.length; i++) {
-            if (nums[i].equals(toSwap)) {
-                nums[i] = swapMax;
-                break;
-            }
-        }
-        for (i = nums.length - 1; i >= 0; i--) {
-            if (nums[i].equals(swapMax)) {
-                nums[i] = toSwap;
-                break;
-            }
+            break;
         }
         int result = 0;
-        for (int x : nums) {
-            result = 10 * result + x;
+        for (int n : arr) {
+            result = 10 * result + n;
         }
         return result;
     }

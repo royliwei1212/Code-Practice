60258646e8d7599f88562936b0c4790c0b54bbc5
Hao Zhou <i@hzhou.me>
2017-10-30 17:36:46 -0400

    update code

--- src/main/java/tech/saltyegg/leetcode/MinimumSizeSubarraySum.java
+++ src/main/java/tech/saltyegg/leetcode/MinimumSizeSubarraySum.java
@@ -8,28 +8,35 @@ package tech.saltyegg.leetcode;
 public class MinimumSizeSubarraySum {
 
     public int minSubArrayLen(int s, int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
+        if (nums == null || nums.length == 0) return 0;
+        int[] sum = new int[nums.length + 1];
+        for (int i = 1; i < sum.length; i++) {
+            if (nums[i - 1] >= s) return 1;
+            sum[i] = sum[i - 1] + nums[i - 1];
         }
-        int length = nums.length;
-        int start, end, crtSum, minLength;
-        start = end = crtSum = 0;
-        minLength = length + 1;
+        if (sum[nums.length] < s) return 0;
+        int result = nums.length;
+        for (int i = 0; i < sum.length - 1; i++) {
+            int x = sum[i];
+            int target = s + x;
+            int loc = search(sum, i + 1, target);
+            if (sum[loc] - sum[i] >= s) result = Math.min(result, loc - i);
+        }
+        return result;
+    }
 
-        while (end < length) {
-
-            while (crtSum < s && end < length) {
-                crtSum += nums[end++];
-            }
-
-            while (crtSum >= s && start < length) {
-                if (end - start < minLength) {
-                    minLength = end - start;
-                }
-                crtSum -= nums[start++];
+    private int search(int[] sum, int l, int target) {
+        int r = sum.length - 1;
+        if (target < sum[l]) return -1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (sum[m] == target) return m;
+            if (sum[m] < target) {
+                l = m + 1;
+            } else {
+                r = m;
             }
         }
-
-        return minLength > length ? 0 : minLength;
+        return r;
     }
 }

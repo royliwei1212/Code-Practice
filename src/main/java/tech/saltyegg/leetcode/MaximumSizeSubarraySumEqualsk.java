4ed372cf1a6b302641914b815c982d417f88c061
Hao Zhou <i@hzhou.me>
2017-10-30 15:06:38 -0400

    code update

--- src/main/java/tech/saltyegg/leetcode/MaximumSizeSubarraySumEqualsk.java
+++ src/main/java/tech/saltyegg/leetcode/MaximumSizeSubarraySumEqualsk.java
@@ -10,31 +10,27 @@ import java.util.Map;
 public class MaximumSizeSubarraySumEqualsk {
 
     public int maxSubArrayLen(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
+        if (nums == null || nums.length == 0) return 0;
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + nums[i - 1];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < dp.length; i++) {
+            if (map.containsKey(dp[i])) continue;
+            map.put(dp[i], i);
         }
 
-        int size = nums.length;
-        Map<Integer, Integer> map = new HashMap<>(size);
-
-        int sum = 0;
-        int max = 0;
-        for (int i = 0; i < size; i++) {
-            int n = nums[i];
-            sum += n;
-            if (sum == k) {
-                max = Math.max(max, i + 1);
-            }
-            if (!map.containsKey(sum)) {
-                map.put(sum, i);
-            }
-
-            int remain = sum - k;
-            if (map.containsKey(remain)) {
-                max = Math.max(max, i - map.get(remain));
+        int result = 0;
+        for (int i = 0; i < dp.length; i++) {
+            int x = dp[i];
+            int y = x - k;
+            if (map.containsKey(y)) {
+                result = Math.max(result, i - map.get(y));
             }
         }
-        return max;
+        return result;
     }
 
 }

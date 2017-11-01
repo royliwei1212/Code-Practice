f5ffc606b02c1c8371fef026f59db4ef413c67c2
Hao Zhou <i@hzhou.me>
2017-11-01 12:18:45 -0400

    code clearup

--- src/main/java/tech/saltyegg/leetcode/MinimumSizeSubarraySum.java
+++ src/main/java/tech/saltyegg/leetcode/MinimumSizeSubarraySum.java
@@ -17,24 +17,21 @@ public class MinimumSizeSubarraySum {
         if (sum[nums.length] < s) return 0;
         int result = nums.length;
         for (int i = 0; i < sum.length - 1; i++) {
-            int x = sum[i];
-            int target = s + x;
-            int loc = search(sum, i + 1, target);
-            if (sum[loc] - sum[i] >= s) result = Math.min(result, loc - i);
+            int l = search(sum, i + 1, sum[i] + s);
+            if (sum[l] - sum[i] >= s) result = Math.min(result, l - i);
         }
-        return result;
+        return result == nums.length + 1 ? 0 : result;
     }
 
     private int search(int[] sum, int l, int target) {
         int r = sum.length - 1;
-        if (target < sum[l]) return -1;
+        if (target < sum[l]) return l;
         while (l < r) {
             int m = l + (r - l) / 2;
-            if (sum[m] == target) return m;
-            if (sum[m] < target) {
-                l = m + 1;
-            } else {
+            if (sum[m] >= target) {
                 r = m;
+            } else {
+                l = m + 1;
             }
         }
         return r;

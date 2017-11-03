0b3848b79147e2a08e5daf717d228e085cdd7457
Hao Zhou <i@hzhou.me>
2017-11-03 17:41:46 -0400

    code improvement

--- src/main/java/tech/saltyegg/leetcode/BasicCalculatorII.java
+++ src/main/java/tech/saltyegg/leetcode/BasicCalculatorII.java
@@ -1,11 +1,6 @@
 package tech.saltyegg.leetcode;
 
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
+import java.util.Stack;
 
 /**
  * Created by hzhou on 2015/6/22. Email: i@hzhou.me
@@ -13,122 +8,42 @@ import static org.junit.Assert.assertEquals;
 public class BasicCalculatorII {
 
     public int calculate(String s) {
-        if (s == null || s.isEmpty()) {
-            return 0;
-        }
-
+        if (s == null || s.isEmpty()) return 0;
+        Stack<Integer> stack = new Stack<>();
         s = s.replaceAll(" ", "");
+        if (s.isEmpty()) return 0;
 
-        List<Integer> list = new ArrayList<Integer>();
+        int num = 0;
+        char f = '+';
+        char c = '+';
 
-        for (int i = 0; i < s.length(); ) {
-            char c = s.charAt(i);
-            int op = isOperator(c);
-            int end = i + 1;
-            if (op == 2) {
-                // for * and /, just calculate the result and store it in the list
-                int a = list.get(list.size() - 1);
-                list.remove(list.size() - 1);
-                end = readNumber(s, i + 1);
-                list.add(getResult(a, Integer.valueOf(s.substring(i + 1, end)), c));
-            } else if (op == 1 && c == '-') {
-                // if minus a number, just store thr negative format number in the list
-                end = readNumber(s, i + 1);
-                list.add(Integer.valueOf(s.substring(i, end)));
+        for (int i = 0; i <= s.length(); i++) {
+            if (i < s.length()) {
+                c = s.charAt(i);
+                if (c >= '0' && c <= '9') {
+                    num = 10 * num + (c - '0');
+                    continue;
+                }
             }
 
-            if (isNumber(c)) {
-                end = readNumber(s, i);
-                list.add(Integer.valueOf(s.substring(i, end)));
+            switch (f) {
+                case '+':
+                    stack.push(num);
+                    break;
+                case '-':
+                    stack.push(-num);
+                    break;
+                case '*':
+                    stack.push(stack.pop() * num);
+                    break;
+                case '/':
+                    stack.push(stack.pop() / num);
+                    break;
             }
-
-            i = end;
+            f = c;
+            num = 0;
         }
 
-        // sum up all integers
-        int result = 0;
-        for (int a : list) {
-            result += a;
-        }
-
-        return result;
-    }
-
-    /**
-     * Read number, and find the last index for current number
-     *
-     * @param s     given string
-     * @param start start index to search number
-     * @return last index of current number
-     */
-    private int readNumber(String s, int start) {
-        while (start < s.length()) {
-            if (!isNumber(s.charAt(start))) {
-                break;
-            }
-            start++;
-        }
-        return start;
-    }
-
-    /**
-     * check a char is number or not
-     *
-     * @param c char to check
-     * @return true for number otherwise false
-     */
-    private boolean isNumber(char c) {
-        return c >= '0' && c <= '9';
-    }
-
-    /**
-     * Check whether it is a operator
-     *
-     * @param c char
-     * @return 2 for (*, /), 1 for (+,-), otherwise 0
-     */
-    private int isOperator(char c) {
-        if (c == '*' || c == '/') {
-            return 2;
-        } else if (c == '+' || c == '-') {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    /**
-     * Calculate the result
-     *
-     * @param x first parameter
-     * @param y second parameter
-     * @param c operator
-     * @return result
-     */
-    private int getResult(int x, int y, char c) {
-        switch (c) {
-            case '+':
-                return x + y;
-            case '-':
-                return x - y;
-            case '*':
-                return x * y;
-            case '/':
-                return x / y;
-            default:
-                return 0;
-        }
-    }
-
-    @Test
-    public void test() {
-        String s = "3+2*2";
-        assertEquals(7, calculate(s));
-
-        s = "3+5 / 2 ";
-        assertEquals(5, calculate(s));
-
-        s = "0" + Integer.MIN_VALUE;
-        assertEquals(Integer.MIN_VALUE, calculate(s));
+        return stack.stream().mapToInt(Integer::intValue).sum();
     }
 }

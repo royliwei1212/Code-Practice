dbf05da8b86acdcb4ccc2881242a8811779416c6
Hao Zhou <i@hzhou.me>
2017-11-03 15:45:02 -0400

    improve code

--- src/main/java/tech/saltyegg/leetcode/PaintHouseII.java
+++ src/main/java/tech/saltyegg/leetcode/PaintHouseII.java
@@ -31,4 +31,33 @@ public class PaintHouseII {
         }
         return result;
     }
+
+    // less time & memory
+    public int minCostII2(int[][] costs) {
+        if (costs == null || costs.length == 0 || costs[0] == null ||
+                costs[0].length == 0 || costs[0].length == 1 && costs.length > 1) return 0;
+
+        if (costs[0].length == 1 && costs.length == 1) return costs[0][0];
+
+        int min1 = 0, min2 = 0, idx = -1;
+        for (int[] c : costs) {
+            int m1 = Integer.MAX_VALUE;
+            int m2 = m1;
+            int id = -1;
+            for (int j = 0; j < costs[0].length; j++) {
+                int cost = c[j] + (j == idx ? min2 : min1);
+                if (cost < m1) {
+                    m2 = m1;
+                    m1 = cost;
+                    id = j;
+                } else if (cost < m2) {
+                    m2 = cost;
+                }
+            }
+            min1 = m1;
+            min2 = m2;
+            idx = id;
+        }
+        return min1;
+    }
 }

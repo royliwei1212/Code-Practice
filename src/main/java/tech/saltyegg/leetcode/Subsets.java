afbe98ecd60cd38fcd74907aaac7b2421754c906
Hao Zhou <i@hzhou.me>
2017-11-04 11:44:35 -0400

    code cleanup

--- src/main/java/tech/saltyegg/leetcode/Subsets.java
+++ src/main/java/tech/saltyegg/leetcode/Subsets.java
@@ -1,12 +1,8 @@
 package tech.saltyegg.leetcode;
 
-import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.Assert.assertSame;
 
 /**
  * Created by zhouhao on 5/20/2015.
@@ -14,13 +10,13 @@ import static org.junit.Assert.assertSame;
 public class Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<List<Integer>> result = new ArrayList<>();
         if (nums == null || nums.length == 0) {
             return result;
         }
 
         Arrays.sort(nums);
-        helper(0, result, new ArrayList<Integer>(), nums);
+        helper(0, result, new ArrayList<>(), nums);
 
         return result;
     }
@@ -31,18 +27,10 @@ public class Subsets {
             return;
         }
 
-        helper(start + 1, result, new ArrayList<Integer>(crt), nums);
+        helper(start + 1, result, new ArrayList<>(crt), nums);
         crt.add(nums[start]);
-        helper(start + 1, result, new ArrayList<Integer>(crt), nums);
+        helper(start + 1, result, new ArrayList<>(crt), nums);
 
     }
-
-    @Test
-    public void test() {
-        int[] nums = new int[]{1, 2, 3};
-        List<List<Integer>> result = subsets(nums);
-        assertSame(8, result.size());
-    }
-
 
 }

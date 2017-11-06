1af17e5ab17c1623bd7219e71a0aa9b6f2f13b72
Hao Zhou <i@hzhou.me>
2017-11-05 20:13:11 -0500

    code cleanup

--- src/main/java/tech/saltyegg/leetcode/FlattenBinaryTreeToLinkedList.java
+++ src/main/java/tech/saltyegg/leetcode/FlattenBinaryTreeToLinkedList.java
@@ -1,59 +1,27 @@
-/**
- * Copyright (c) 2015 hzhou, All rights reserved.
- */
 package tech.saltyegg.leetcode;
 
-import org.junit.Test;
 import tech.saltyegg.leetcode.parent.TreeNode;
 
 import java.util.Stack;
 
-/**
- * Description
- *
- * @author hzhou
- */
 public class FlattenBinaryTreeToLinkedList {
 
     public void flatten(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode cursor = new TreeNode(0);
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
         while (!stack.isEmpty()) {
-            cursor.right = stack.pop();
-            cursor.left = null;
-            cursor = cursor.right;
+            TreeNode node = stack.pop();
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+            node.left = null;
 
-            if (cursor.right != null) {
-                stack.push(cursor.right);
-            }
-
-            if (cursor.left != null) {
-                cursor.right = cursor.left;
-                if (cursor.left.right != null) {
-                    stack.push(cursor.left.right);
-                }
-                if (cursor.left.left != null) {
-                    stack.push(cursor.left.left);
-                }
-                cursor.left = null;
-                cursor = cursor.right;
+            if (stack.isEmpty()) {
+                node.right = null;
+            } else {
+                node.right = stack.peek();
             }
         }
     }
 
-    @Test
-    public void test() {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        /*root.right = new TreeNode(5);
-		root.left.left = new TreeNode(3);
-		root.left.right = new TreeNode(4);
-		root.right.right = new TreeNode(6);
-		*/
-        flatten(root);
-    }
 }

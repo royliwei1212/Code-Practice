401f0c489a18ae851dab256a3cb40a9976dc0458
Hao Zhou <hzhou.me@gmail.com>
2017-11-13 23:53:35 -0500

    add Longest Word in Dictionary

---
+++ src/main/java/tech/saltyegg/leetcode/LongestWordInDictionary.java
@@ -0,0 +1,33 @@
+package tech.saltyegg.leetcode;
+
+import java.util.*;
+
+public class LongestWordInDictionary {
+    public String longestWord(String[] words) {
+        if (words == null || words.length == 0) return "";
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (String w : words) {
+            int len = w.length();
+            Set<String> set = map.getOrDefault(len, new TreeSet<>(Comparator.reverseOrder()));
+            set.add(w);
+            map.put(len, set);
+        }
+        if (!map.containsKey(1)) return "";
+        String result = "";
+        for (String s : map.get(1)) {
+            Queue<String> queue = new LinkedList<>();
+            queue.add(s);
+            String max = s;
+            while (!queue.isEmpty()) {
+                max = queue.poll();
+                if (!map.containsKey(max.length() + 1)) continue;
+                for (String x : map.get(max.length() + 1)) {
+                    if (x.startsWith(max)) queue.add(x);
+                }
+            }
+            if (max.length() > result.length() || max.length() == result.length() && max.compareTo(result) < 0) result = max;
+        }
+
+        return result;
+    }
+}

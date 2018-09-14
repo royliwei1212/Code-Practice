9ee2fc5184a1b538f48b791fcafc43eeb91d8bc3
Hao Zhou <i@hzhou.me>
2018-09-13 21:15:22 -0400

    can pass now

--- src/main/java/tech/saltyegg/leetcode/ConsecutiveNumbersSum.java
+++ src/main/java/tech/saltyegg/leetcode/ConsecutiveNumbersSum.java
@@ -4,7 +4,7 @@ public class ConsecutiveNumbersSum {
 
     public int consecutiveNumbersSum(int N) {
         int result = 0;
-        for (int i = 1; i <= 2 * N; i++) {
+        for (int i = 1; i <= (int) Math.sqrt(2 * N); i++) {
             if ((2 * N % i) != 0) continue;
             int x = 2 * N / i - i - 1;
             if (x % 2 == 0 && x > 0) {

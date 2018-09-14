189676d7c454fb9ed93c138a30e8f289670af541
Hao Zhou <i@hzhou.me>
2018-09-13 21:05:22 -0400

    Time Limit Exceeded

---
+++ src/main/java/tech/saltyegg/leetcode/ConsecutiveNumbersSum.java
@@ -0,0 +1,17 @@
+package tech.saltyegg.leetcode;
+
+public class ConsecutiveNumbersSum {
+
+    public int consecutiveNumbersSum(int N) {
+        int result = 0;
+        for (int i = 1; i <= 2 * N; i++) {
+            if ((2 * N % i) != 0) continue;
+            int x = 2 * N / i - i - 1;
+            if (x % 2 == 0 && x > 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+}

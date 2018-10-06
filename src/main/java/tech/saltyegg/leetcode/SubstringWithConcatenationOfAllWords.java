e7f1a8f7761c6d07403a0ba8b9e960d1c4b50cad
Hao Zhou <i@hzhou.me>
2018-10-06 15:06:15 -0400

    update 30. Substring with Concatenation of All Words

--- src/main/java/tech/saltyegg/leetcode/SubstringWithConcatenationOfAllWords.java
+++ src/main/java/tech/saltyegg/leetcode/SubstringWithConcatenationOfAllWords.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Test;
-
 /**
  * Created by hzhou on 2015/5/28. Email: codeashobby@gmail.com
  * <p/>
@@ -15,65 +13,32 @@ import org.junit.Test;
 public class SubstringWithConcatenationOfAllWords {
 
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> result = new ArrayList<Integer>();
-        if (s == null || words == null) {
-            return result;
-        }
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.isEmpty() || words == null || words.length == 0) return result;
 
-        int wordLength = words[0].length();
-        int totalLength = wordLength * words.length;
-        if (s.length() < totalLength) {
-            return result;
-        }
-        Map<String, Integer> map = getMap(words);
-        for (int i = 0; i <= s.length() - totalLength; i++) {
-            String tmp = s.substring(i, i + wordLength);
-            if (map.containsKey(tmp) && check(s.substring(i, i + totalLength), words, map)) {
-                result.add(i);
-            }
-        }
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) map.put(w, map.getOrDefault(w, 0) + 1);
 
+        int sl = words[0].length();
+        int tl = sl * words.length;
+
+        for (int i = 0; i <= s.length() - tl; i++) {
+            String sub = s.substring(i, i + sl);
+            if (!map.containsKey(sub)) continue;
+            if (helper(s.substring(i, i + tl), sl, map)) result.add(i);
+        }
         return result;
     }
 
-    private Map<String, Integer> getMap(String[] words) {
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        for (String s : words) {
-            if (map.containsKey(s)) {
-                map.put(s, map.get(s) + 1);
-            } else {
-                map.put(s, 1);
-            }
-        }
-        return map;
-    }
-
-    private boolean check(String s, String[] words, Map<String, Integer> map) {
-        Map<String, Integer> tMap = new HashMap<String, Integer>();
-        int length = words[0].length();
-        for (int i = 0; i <= s.length() - length; i = i + length) {
-            String tmp = s.substring(i, i + length);
-            if (!map.containsKey(tmp)) {
-                return false;
-            }
-
-            if (tMap.containsKey(tmp)) {
-                tMap.put(tmp, tMap.get(tmp) + 1);
-            } else {
-                tMap.put(tmp, 1);
-            }
-            if (tMap.get(tmp) > map.get(tmp)) {
-                return false;
-            }
+    private boolean helper(String s, int len, Map<String, Integer> map) {
+        Map<String, Integer> count = new HashMap<>();
+        for (int i = 0; i < s.length(); i += len) {
+            String sub = s.substring(i, i + len);
+            if (!map.containsKey(sub)) return false;
+            int c = count.getOrDefault(sub, 0) + 1;
+            if (c > map.get(sub)) return false;
+            count.put(sub, c);
         }
         return true;
     }
-
-    @Test
-    public void test() {
-        String s = "lingmindraboofooowingdingbarrwingmonkeypoundcake";
-        String[] words = new String[]{"fooo", "barr", "wing", "ding", "wing"};
-        List<Integer> result = findSubstring(s, words);
-    }
-
 }

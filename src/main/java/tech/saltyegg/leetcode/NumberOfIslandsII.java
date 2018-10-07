38f339d193b66406fa80061e86d4764eb8cced9f
Hao Zhou <i@hzhou.me>
2018-10-07 15:12:21 -0400

    update 305. Number of Islands II

--- src/main/java/tech/saltyegg/leetcode/NumberOfIslandsII.java
+++ src/main/java/tech/saltyegg/leetcode/NumberOfIslandsII.java
@@ -1,36 +1,37 @@
 package tech.saltyegg.leetcode;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 // Union find
 public class NumberOfIslandsII {
 
     public List<Integer> numIslands2(int m, int n, int[][] positions) {
         List<Integer> result = new ArrayList<>();
-        if (positions == null || positions.length == 0) return result;
+        if (positions == null || positions.length == 0 || m <= 0 || n <= 0) return result;
 
-        Map<Integer, Integer> map = new HashMap<>();
-        int[][] direction = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        QuickUnion qu = new QuickUnion(m * n);
+        int[][] dir = new int[][]{
+                {1, 0},
+                {-1, 0},
+                {0, -1},
+                {0, 1}
+        };
         int count = 0;
-
-        for (int[] p : positions) {
-            int x = p[0];
-            int y = p[1];
-            map.put(n * x + y, n * x + y);
+        for (int[] pos : positions) {
+            int a = pos[0];
+            int b = pos[1];
+            int cb = a * n + b;
+            qu.id[cb] = cb;
             count++;
-            for (int[] d : direction) {
-                int xx = x + d[0];
-                int yy = y + d[1];
-                int pos = xx * n + yy;
-                if (xx < 0 || yy < 0 || xx >= m || yy >= n || !map.containsKey(pos)) continue;
-                int rx = findRoot(n * x + y, map);
-                int ry = findRoot(pos, map);
-                if (rx != ry) {
+            for (int[] d : dir) {
+                int x = a + d[0];
+                int y = b + d[1];
+                int nb = x * n + y;
+                if (x < 0 || y < 0 || x >= m || y >= n || qu.id[nb] == -1) continue;
+                if (!qu.con(cb, nb)) {
+                    qu.union(cb, nb);
                     count--;
-                    map.put(rx, ry);
                 }
             }
             result.add(count);
@@ -38,17 +39,28 @@ public class NumberOfIslandsII {
         return result;
     }
 
-    private int findRoot(int p, Map<Integer, Integer> map) {
-        int root = map.get(p);
-        while (root != map.get(root)) {
-            root = map.get(root);
+    class QuickUnion {
+
+        int[] id;
+
+        QuickUnion(int n) {
+            id = new int[n];
+            for (int i = 0; i < n; i++) id[i] = -1;
         }
 
-        while (p != map.get(p)) {
-            int t = map.get(p);
-            map.put(p, root);
-            p = t;
+        private int root(int i) {
+            while (i != id[i]) i = id[i];
+            return i;
         }
-        return root;
+
+        public boolean con(int p, int q) {
+            return root(p) == root(q);
+        }
+
+        public void union(int p, int q) {
+            int ip = root(p);
+            int iq = root(q);
+            id[ip] = iq;
+        }
     }
 }

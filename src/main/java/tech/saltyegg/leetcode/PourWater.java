6eae2d7908fc50544d95399a018fdb91bc9316d4
Hao Zhou <i@hzhou.me>
2018-10-09 00:23:05 -0400

    add 755. Pour Water

---
+++ src/main/java/tech/saltyegg/leetcode/PourWater.java
@@ -0,0 +1,41 @@
+package tech.saltyegg.leetcode;
+
+public class PourWater {
+
+    public int[] pourWater(int[] heights, int V, int K) {
+        if (heights == null || heights.length == 0 || K >= heights.length || K < 0 || V <= 0) return heights;
+
+        while (V-- > 0) {
+            int t = K;
+            for (int i = K - 1; i >= 0; i--) {
+                while (i >= 0 && heights[i] == heights[t]) i--;
+                if (i < 0) break;
+                if (heights[i] < heights[t]) {
+                    t = i;
+                } else {
+                    break;
+                }
+            }
+
+            if (t < K) {
+                heights[t] += 1;
+                continue;
+            }
+
+            for (int i = K + 1; i < heights.length; i++) {
+                while (i < heights.length && heights[i] == heights[t]) i++;
+                if (i == heights.length) break;
+                if (heights[i] < heights[t]) {
+                    t = i;
+                } else {
+                    break;
+                }
+            }
+            heights[t] += 1;
+
+        }
+        return heights;
+
+    }
+
+}

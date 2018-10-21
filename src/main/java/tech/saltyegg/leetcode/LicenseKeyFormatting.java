1d3289311b8ea6ecd7ebafcd5076cf839bab9caa
Hao Zhou <i@hzhou.me>
2018-10-20 21:52:10 -0700

    update 482. License Key Formatting

--- src/main/java/tech/saltyegg/leetcode/LicenseKeyFormatting.java
+++ src/main/java/tech/saltyegg/leetcode/LicenseKeyFormatting.java
@@ -7,20 +7,21 @@ package tech.saltyegg.leetcode;
 public class LicenseKeyFormatting {
 
     public String licenseKeyFormatting(String S, int K) {
-        if (S == null || S.isEmpty()) {
-            return "";
+        if (S == null || S.isEmpty()) return "";
+        S = S.toUpperCase().replace("-", "");
+        if (S.isEmpty()) return "";
+        StringBuilder sb = new StringBuilder();
+        int len = S.length();
+        int start = 0;
+        while (len > 0) {
+            int l = len % K;
+            if (l == 0) l = K;
+            sb.append("-").append(S, start, start + l);
+            start += l;
+            len -= l;
         }
-
-        String str = S.replaceAll("-", "").toUpperCase();
-        if (str.length() <= K) {
-            return str;
-        }
-
-        int batch = str.length() / K;
-        int offset = str.length() - batch * K;
-        String prefix = str.substring(0, offset);
-        String suffix = str.substring(offset).replaceAll("([A-Za-z0-9]{" + K + "})", "-$1");
-        return prefix.isEmpty() ? suffix.substring(1) : prefix + suffix;
+        sb.deleteCharAt(0);
+        return sb.toString();
     }
 
 }

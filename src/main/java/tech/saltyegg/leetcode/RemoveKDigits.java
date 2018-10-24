2d4327cdae143da3a0c6d83524d5c93f4db7f032
Hao Zhou <i@hzhou.me>
2018-10-23 23:14:06 -0400

    update 402. Remove K Digits

--- src/main/java/tech/saltyegg/leetcode/RemoveKDigits.java
+++ src/main/java/tech/saltyegg/leetcode/RemoveKDigits.java
@@ -9,35 +9,29 @@ import java.util.Stack;
 public class RemoveKDigits {
 
     public String removeKdigits(String num, int k) {
-        if (num == null || num.isEmpty() || num.length() < k) {
-            return null;
-        }
+        if (num == null || num.length() == 0) return "0";
 
-        if (num.length() == k) return "0";
-        int[] arr = new int[num.length()];
-        for (int i = 0; i < num.length(); i++) {
-            arr[i] = num.charAt(i) - '0';
-        }
-
-        int length = num.length() - k;
         Stack<Integer> stack = new Stack<>();
-
-        stack.push(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            while (!stack.isEmpty() && stack.size() + num.length() - i > length && arr[i] < stack.peek()) {
+        for (int i = 0; i < num.length(); i++) {
+            int n = num.charAt(i) - '0';
+            while (k > 0 && !stack.isEmpty() && stack.peek() > n) {
                 stack.pop();
+                k--;
             }
+            stack.push(n);
+        }
 
-            if (stack.size() < length) {
-                stack.push(arr[i]);
-            }
+        while (k > 0 && !stack.isEmpty()) {
+            stack.pop();
+            k--;
         }
 
         StringBuilder sb = new StringBuilder();
-        stack.forEach(sb::append);
-        while (sb.charAt(0) == '0' && sb.length() > 1) {
-            sb.deleteCharAt(0);
+        for (int n : stack) {
+            if (n == 0 && sb.length() == 0) continue;
+            sb.append(n);
         }
+        if (sb.length() == 0) sb.append("0");
         return sb.toString();
     }
 

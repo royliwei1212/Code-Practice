b588efc2c42cdf1e56a42251c9a2ba368a9cbca7
Hao Zhou <i@hzhou.me>
2018-10-28 16:47:35 -0400

    add 399. Evaluate Division

---
+++ src/main/java/tech/saltyegg/leetcode/EvaluateDivision.java
@@ -0,0 +1,47 @@
+package tech.saltyegg.leetcode;
+
+import java.util.*;
+
+public class EvaluateDivision {
+
+    public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+        Map<String, List<String>> eMap = new HashMap<>();
+        Map<String, List<Double>> vMap = new HashMap<>();
+
+        for (int i = 0; i < equations.length; i++) {
+            String a = equations[i][0], b = equations[i][1];
+            eMap.putIfAbsent(a, new ArrayList<>());
+            eMap.putIfAbsent(b, new ArrayList<>());
+            vMap.putIfAbsent(a, new ArrayList<>());
+            vMap.putIfAbsent(b, new ArrayList<>());
+
+            eMap.get(a).add(b);
+            eMap.get(b).add(a);
+            vMap.get(a).add(values[i]);
+            vMap.get(b).add(1.0 / values[i]);
+        }
+        double[] result = new double[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            result[i] = helper(eMap, vMap, queries[i][0], queries[i][1], new HashSet<>(), 1.0);
+            if (result[i] == 0.0) result[i] = -1.0;
+        }
+        return result;
+    }
+
+    private double helper(Map<String, List<String>> eMap, Map<String, List<Double>> vMap, String start, String end, Set<String> visited, double value) {
+        if (visited.contains(start) || !eMap.containsKey(start)) return 0.0;
+        if (start.equals(end)) return value;
+
+        visited.add(start);
+        List<String> nextStr = eMap.get(start);
+        List<Double> nextVal = vMap.get(start);
+        double result = 1.0;
+        for (int i = 0; i < nextStr.size(); i++) {
+            result = helper(eMap, vMap, nextStr.get(i), end, visited, value * nextVal.get(i));
+            if (result != 0.0) break;
+        }
+
+        visited.remove(start);
+        return result;
+    }
+}

b923cd0e54f9a18d08a7a583a69bda4bd6e5b797
Hao Zhou <i@hzhou.me>
2018-10-31 00:19:09 -0400

    update 359. Logger Rate Limiter

--- src/main/java/tech/saltyegg/leetcode/LoggerRateLimiter.java
+++ src/main/java/tech/saltyegg/leetcode/LoggerRateLimiter.java
@@ -1,28 +1,34 @@
 package tech.saltyegg.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LoggerRateLimiter {
 
-    private Map<String, Integer> cache;
+    private int[] bucket;
+    private Set[] set;
 
     public LoggerRateLimiter() {
-        cache = new HashMap<>();
+        bucket = new int[10];
+        set = new Set[10];
+        for (int i = 0; i < 10; i++) {
+            set[i] = new HashSet();
+        }
     }
 
-    /**
-     * Returns true if the message should be printed in the given timestamp, otherwise returns false.
-     * If this method returns false, the message will not be printed.
-     * The timestamp is in seconds granularity.
-     */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        int time = cache.getOrDefault(message, -100);
-        if (timestamp - time >= 10) {
-            cache.put(message, timestamp);
-            return true;
+        int ts = timestamp % 10;
+        if (timestamp != bucket[ts]) {
+            set[ts].clear();
+            bucket[ts] = timestamp;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (timestamp - bucket[i] < 10 && set[i].contains(message)) {
+                return false;
+            }
         }
 
-        return false;
+        set[ts].add(message);
+        return true;
     }
 }

5dad4827f1d77816cd4a4a5b8d8b28c60e535d1f
Hao Zhou <i@hzhou.me>
2018-11-03 17:07:27 -0400

    update 486. Predict the Winner

--- src/main/java/tech/saltyegg/leetcode/PredictTheWinner.java
+++ src/main/java/tech/saltyegg/leetcode/PredictTheWinner.java
@@ -7,10 +7,22 @@ public class PredictTheWinner {
     }
 
     public int helper(int left, int right, int[] nums) {
-        if (left == right) {
-            return nums[left];
-        }
+        if (left == right) return nums[left];
+        return Math.max(nums[left] - helper(left + 1, right, nums),
+                nums[right] - helper(left, right - 1, nums));
+    }
 
-        return Math.max(nums[left] - helper(left + 1, right, nums), nums[right] - helper(left, right - 1, nums));
+    public boolean PredictTheWinner2(int[] nums) {
+        int len = nums.length;
+        if (len % 2 == 0) return true;
+        int[] dp = new int[len];
+
+        for (int i = len - 1; i >= 0; i--) {
+            dp[i] = nums[i];
+            for (int j = i + 1; j < len; j++) {
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp[len - 1] >= 0;
     }
 }

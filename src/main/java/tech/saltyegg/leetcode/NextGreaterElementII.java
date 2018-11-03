955c9b2e2594f1798fd3cab807256d32fb6637ce
Hao Zhou <i@hzhou.me>
2018-11-02 23:16:47 -0400

    update nextGreaterElements

--- src/main/java/tech/saltyegg/leetcode/NextGreaterElementII.java
+++ src/main/java/tech/saltyegg/leetcode/NextGreaterElementII.java
@@ -1,36 +1,22 @@
 package tech.saltyegg.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class NextGreaterElementII {
 
     public int[] nextGreaterElements(int[] nums) {
-        if (nums == null || nums.length == 0) return new int[0];
-        int max = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] >= nums[max]) {
-                max = i;
-            }
-        }
 
-        int x = max + nums.length;
+        Stack<Integer> stack = new Stack<>();
         int[] result = new int[nums.length];
-        result[max] = -1;
-        List<Integer> queue = new ArrayList<>();
-        queue.add(nums[max]);
-        for (int i = x - 1; i > max; i--) {
-            int index = i % nums.length;
-            if (nums[index] == nums[max]) result[index] = -1;
-            for (int n = queue.size() - 1; n >= 0; n--) {
-                if (queue.get(n) > nums[index]) {
-                    result[index] = queue.get(n);
-                    break;
-                }
+        for (int i = 0; i < 2 * nums.length; i++) {
+            while (!stack.isEmpty() && nums[i % nums.length] > nums[stack.peek()]) {
+                result[stack.pop()] = nums[i % nums.length];
             }
-            if (nums[index] > nums[(i - 1) % nums.length]) queue.add(nums[index]);
-
+            if (i < nums.length) stack.push(i);
         }
+
+        for (int n : stack) result[n] = -1;
+
         return result;
     }
 }

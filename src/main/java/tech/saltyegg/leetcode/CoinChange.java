7f558f3e75d57b7552c310ffb55a29da972ad42b
Hao Zhou <i@hzhou.me>
2018-11-03 23:20:12 -0400

    update 322. Coin Change

--- src/main/java/tech/saltyegg/leetcode/CoinChange.java
+++ src/main/java/tech/saltyegg/leetcode/CoinChange.java
@@ -1,39 +1,16 @@
 package tech.saltyegg.leetcode;
 
-import java.util.Arrays;
-
 public class CoinChange {
 
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || coins.length < 1) return -1;
-        Arrays.sort(coins);
-        if (amount == 0) return 0;
-        if (coins[0] > amount) return -1;
-
-        int[] dict = new int[amount + 1];
-        for (int i = 0; i < dict.length; i++) {
-            dict[i] = -1;
-        }
-        for (int i : coins) {
-            if (i <= amount) {
-                dict[i] = 1;
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) dp[i] = amount + 1;
+        for (int i = 1; i <= amount; i++) {
+            for (int c : coins) {
+                if (c <= i) dp[i] = Math.min(dp[i], dp[i - c] + 1);
             }
         }
-        for (int i = coins[0] + 1; i <= amount; i++) {
-            int x = dict[i] != -1 ? Math.min(dict[i], findMin(coins, dict, i)) : findMin(coins, dict, i);
-            dict[i] = x == Integer.MAX_VALUE ? -1 : x;
-        }
-        return dict[amount];
-    }
-
-    private int findMin(int[] coins, int[] dict, int index) {
-        int result = Integer.MAX_VALUE;
-        for (int i : coins) {
-            if (index - i >= 0 && dict[index - i] > 0) {
-                result = Math.min(result, dict[index - i] + 1);
-            }
-        }
-        return result;
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 
 }

449180e9ed8c778209a904132ced5eeb59eb8c39
Hao Zhou <i@hzhou.me>
2018-11-03 21:52:59 -0400

    update 399. Evaluate Division

--- src/main/java/tech/saltyegg/leetcode/EvaluateDivision.java
+++ src/main/java/tech/saltyegg/leetcode/EvaluateDivision.java
@@ -1,6 +1,11 @@
 package tech.saltyegg.leetcode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class EvaluateDivision {
 
@@ -41,6 +46,46 @@ public class EvaluateDivision {
             if (result != 0.0) break;
         }
 
+        visited.remove(start);
+        return result;
+    }
+
+    public double[] calcEquation2(String[][] equations, double[] values, String[][] queries) {
+        Map<String, Map<String, Double>> map = new HashMap<>();
+        for (int i = 0; i < equations.length; i++) {
+            String[] e = equations[i];
+            double v = values[i];
+            map.putIfAbsent(e[0], new HashMap<>());
+            map.putIfAbsent(e[1], new HashMap<>());
+            map.get(e[0]).put(e[1], v);
+            map.get(e[1]).put(e[0], 1 / v);
+        }
+
+        double[] result = new double[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String[] q = queries[i];
+            if (!map.containsKey(q[0]) || !map.containsKey(q[1])) {
+                result[i] = -1;
+                continue;
+            }
+            result[i] = helper(map, new HashSet<>(), q[0], q[1], 1);
+            if (result[i] == 0) result[i] = -1;
+        }
+
+        return result;
+    }
+
+    private double helper(Map<String, Map<String, Double>> map, Set<String> visited, String start, String end, double r) {
+        if (visited.contains(start)) return 0;
+        if (start.equals(end)) {
+            return r;
+        }
+        double result = 0;
+        visited.add(start);
+        for (Map.Entry<String, Double> e : map.get(start).entrySet()) {
+            result = helper(map, visited, e.getKey(), end, r * e.getValue());
+            if (result != 0) break;
+        }
         visited.remove(start);
         return result;
     }

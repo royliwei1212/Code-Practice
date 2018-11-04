62439e55a8092f50c4372df166d2b4a42652c803
Hao Zhou <i@hzhou.me>
2018-11-04 13:38:57 -0500

    update 413. Arithmetic Slices

--- src/main/java/tech/saltyegg/leetcode/ArithmeticSlices.java
+++ src/main/java/tech/saltyegg/leetcode/ArithmeticSlices.java
@@ -25,4 +25,17 @@ public class ArithmeticSlices {
         return result;
     }
 
+    public int numberOfArithmeticSlices2(int[] A) {
+        if (A == null || A.length < 3) return 0;
+        int[] dp = new int[A.length];
+
+        for (int i = 2; i < A.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+        int result = 0;
+        for (int i : dp) result += i;
+        return result;
+    }
 }

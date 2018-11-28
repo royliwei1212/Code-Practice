d03d06a8b2c864063b8796d04f68c31139fc0414
Hao Zhou <i@hzhou.me>
2018-11-27 23:46:03 -0500

    update 37. Sudoku Solver

--- src/main/java/tech/saltyegg/leetcode/SudokuSolver.java
+++ src/main/java/tech/saltyegg/leetcode/SudokuSolver.java
@@ -8,28 +8,21 @@ package tech.saltyegg.leetcode;
 public class SudokuSolver {
 
     public void solveSudoku(char[][] board) {
-        if (board == null || board.length != 9) {
-            return;
-        }
+        if (board == null || board.length != 9) return;
         helper(board);
     }
 
     private boolean helper(char[][] board) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char v = '1'; v <= '9'; v++) {
-                        if (isValid(board, i, j, v)) {
-                            board[i][j] = v;
-                            if (helper(board)) {
-                                return true;
-                            } else {
-                                board[i][j] = '.';
-                            }
-                        }
-                    }
-                    return false;
+                if (board[i][j] != '.') continue;
+                for (char v = '1'; v <= '9'; v++) {
+                    if (!isValid(board, i, j, v)) continue;
+                    board[i][j] = v;
+                    if (helper(board)) return true;
+                    board[i][j] = '.';
                 }
+                return false;
             }
         }
         return true;
@@ -37,18 +30,14 @@ public class SudokuSolver {
 
     private boolean isValid(char[][] board, int x, int y, char c) {
         for (int i = 0; i < 9; i++) {
-            if (board[x][i] == c || board[i][y] == c) {
-                return false;
-            }
+            if (board[x][i] == c || board[i][y] == c) return false;
         }
 
         int xStart = x / 3 * 3;
         int yStart = y / 3 * 3;
         for (int i = xStart; i < xStart + 3; i++) {
             for (int j = yStart; j < yStart + 3; j++) {
-                if (board[i][j] == c) {
-                    return false;
-                }
+                if (board[i][j] == c) return false;
             }
         }
 
